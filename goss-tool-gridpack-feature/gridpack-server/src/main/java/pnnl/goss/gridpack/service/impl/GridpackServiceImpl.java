06c84f1026201e0a33ae206c9cbdfca400ccd19d
Craig Allwardt <craig.allwardt@pnnl.gov>
2014-04-21 12:36:03 -0700

    Moved implementation to an impl directory.

--- goss-tool-gridpack-feature/gridpack-server/src/main/java/pnnl/goss/gridpack/service/impl/GridpackServiceImpl.java
+++ goss-tool-gridpack-feature/gridpack-server/src/main/java/pnnl/goss/gridpack/service/impl/GridpackServiceImpl.java
@@ -42,7 +42,7 @@
     operated by BATTELLE for the UNITED STATES DEPARTMENT OF ENERGY
     under Contract DE-AC05-76RL01830
 */
-package pnnl.goss.gridpack.service;
+package pnnl.goss.gridpack.service.impl;
 
 import java.util.Collection;
 
