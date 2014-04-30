1521e8299000636e3c71337d6d0d1b4ffb074c45
Craig Allwardt <craig.allwardt@pnnl.gov>
2014-04-29 17:12:17 -0700

    Updated ui for modifying the dsa options.

--- goss-dsa-feature/dsa-web/src/main/java/pnnl/goss/dsa/web/GossDsaServlet.java
+++ goss-dsa-feature/dsa-web/src/main/java/pnnl/goss/dsa/web/GossDsaServlet.java
@@ -2,6 +2,7 @@ package pnnl.goss.dsa.web;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -9,6 +10,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class GossDsaServlet extends HttpServlet {
+	static String PARAM_TIME_OPTION = "timeOption";
+	static String PARAM_TIME_OFFSET = "offset";
+	static String TIME_OPTION_CURRENT = "currentTime";
+	static String TIME_OPTION_OFFSET = "currentTimeOffset";
+	
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
@@ -23,7 +29,12 @@ public class GossDsaServlet extends HttpServlet {
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		super.doPost(req, resp);
+
+		Map params = req.getParameterMap();
+		PrintWriter out = resp.getWriter();
+		
+		out.write("Time offset option: " + req.getParameter(PARAM_TIME_OPTION) + " " + req.getParameter(PARAM_TIME_OPTION).equals(TIME_OPTION_CURRENT) + "<br />\n");
+		out.write("Time offset value: " + req.getParameter(PARAM_TIME_OFFSET) + "<br />\n");
+		
 	}
 }
--- goss-dsa-feature/dsa-web/src/main/webapp/index.jsp
+++ goss-dsa-feature/dsa-web/src/main/webapp/index.jsp
@@ -2,14 +2,14 @@
 <body>
 	<h2>DSA Management</h2>
 	
-	<form action="changeOption" method="post">
+	<form action="dsa-service/update" method="post">
 	<table>
 		<caption>DSA Display Time</caption>
 		<tr>
 			<td>Option:</td>
 			<td>
-				<input type="radio" name="timeOption" id="currentTime" value="checked" /> Current Time<br/>
-				<input type="radio" name="timeOption" id="currentTimeOffset" /> Current Time Offset: <input type="text" name="offset" id="offset" /><br />
+				<input type="radio" name="timeOption" id="currentTime" value="currentTime" checked="checked" /> Current Time<br/>
+				<input type="radio" name="timeOption" id="currentTimeOffset" value="currentTimeOffset" /> Current Time Offset: <input type="text" name="offset" id="offset" /> (+/- hh:mm ex 08:00 for +8 hours)<br />
 			</td>
 		</tr>
 		<tr>
