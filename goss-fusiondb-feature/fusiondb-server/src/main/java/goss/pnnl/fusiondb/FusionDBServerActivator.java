ff4aeff912fb9001fa6119f0280a6c1e324455f5
Poorva Sharma <poorva.sharma@pnnl.gov>
2014-07-25 11:20:26 -0700

    updated plugin to include ipojo and updated activator

--- goss-fusiondb-feature/fusiondb-server/src/main/java/goss/pnnl/fusiondb/FusionDBServerActivator.java
+++ goss-fusiondb-feature/fusiondb-server/src/main/java/goss/pnnl/fusiondb/FusionDBServerActivator.java
@@ -7,16 +7,15 @@ import goss.pnnl.fusiondb.handlers.RequestHAInterchangeScheduleHandler;
 import goss.pnnl.fusiondb.handlers.RequestRTEDScheduleHandler;
 
 import java.util.Dictionary;
-import java.util.Hashtable;
 
-import org.osgi.framework.BundleActivator;
+import org.apache.felix.ipojo.annotations.Component;
+import org.apache.felix.ipojo.annotations.Instantiate;
+import org.apache.felix.ipojo.annotations.Invalidate;
+import org.apache.felix.ipojo.annotations.Requires;
+import org.apache.felix.ipojo.annotations.Updated;
+import org.apache.felix.ipojo.annotations.Validate;
 import org.osgi.framework.BundleContext;
-import org.osgi.framework.Constants;
-import org.osgi.framework.Filter;
-import org.osgi.framework.InvalidSyntaxException;
 import org.osgi.service.cm.ConfigurationException;
-import org.osgi.service.cm.ManagedService;
-import org.osgi.util.tracker.ServiceTracker;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -28,14 +27,16 @@ import pnnl.goss.fusiondb.requests.RequestRTEDSchedule;
 import pnnl.goss.security.core.authorization.basic.AccessControlHandlerAllowAll;
 import pnnl.goss.server.core.GossRequestHandlerRegistrationService;
 
-public class FusionDBServerActivator implements BundleActivator, ManagedService{
+@Component(managedservice="pnnl.goss.fusiondb.server")
+@Instantiate
+public class FusionDBServerActivator {
 
 	/**
 	 * <p>
 	 * The configuration file in $SMX_HOME/etc will be CONFIG_PID.cfg
 	 * </p>
 	 */
-	private static final String CONFIG_PID = "pnnl.goss.fusiondb.server";
+	//private static final String CONFIG_PID = "pnnl.goss.fusiondb.server";
 		
 	/**
 	 * <p>
@@ -49,46 +50,37 @@ public class FusionDBServerActivator implements BundleActivator, ManagedService{
 	 * Allows tracking of the registration service from the core-server.
 	 * </p>
 	 */
-	private ServiceTracker registrationTracker;
+	//private ServiceTracker registrationTracker;
 	
-	@Override
-	public void start(BundleContext context) throws Exception {
+	@Requires
+	private GossRequestHandlerRegistrationService registrationService;
+
+	@Validate
+	public void start() {
 		System.out.println("Starting bundle"+this.getClass().getName());
 		log.info("Starting bundle: " + this.getClass().getName());
-		try {
-			String filterStr = "(" + Constants.OBJECTCLASS + "=" + GossRequestHandlerRegistrationService.class.getName() + ")";
-			Filter filter = context.createFilter(filterStr);
-			registrationTracker = new ServiceTracker(context, filter, null);
-			registrationTracker.open();
+		if(registrationService != null){
+			registrationService.addHandlerMapping(RequestActualTotal.class, RequestActualTotalHandler.class);
+			registrationService.addHandlerMapping(RequestCapacityRequirement.class, RequestCapacityRequirementHandler.class);
+			registrationService.addHandlerMapping(RequestForecastTotal.class, RequestForecastTotalHandler.class);
+			registrationService.addHandlerMapping(RequestHAInterchangeSchedule.class, RequestHAInterchangeScheduleHandler.class);
+			registrationService.addHandlerMapping(RequestRTEDSchedule.class, RequestRTEDScheduleHandler.class);
 			
-			// Register the handlers on the registration service.
-			registerRequestAndHandlers();
-			
-		} catch (InvalidSyntaxException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			//-------------------------------------Fusion Security----------------------------------------------
+			registrationService.addSecurityMapping(RequestActualTotal.class, AccessControlHandlerAllowAll.class);
+			registrationService.addSecurityMapping(RequestCapacityRequirement.class, AccessControlHandlerAllowAll.class);
+			registrationService.addSecurityMapping(RequestForecastTotal.class, AccessControlHandlerAllowAll.class);
+			registrationService.addSecurityMapping(RequestHAInterchangeSchedule.class, AccessControlHandlerAllowAll.class);
+			registrationService.addSecurityMapping(RequestRTEDSchedule.class, AccessControlHandlerAllowAll.class);
+		}else{
+			log.debug(GossRequestHandlerRegistrationService.class.getName()+" not found!");
 		}
+	}
 
-		// Register the service for receiving updates to the configuration file
-		context.registerService(ManagedService.class.getName(), this, getDefaults());
-		
-	}
-	
-	@SuppressWarnings({ "rawtypes", "unchecked" })
-	private Hashtable getDefaults(){
-		Hashtable properties= new Hashtable();
-		properties.put(Constants.SERVICE_PID,  CONFIG_PID);
-		//properties.put("datasource0", "datasource0=northandsouth,jdbc:mysql://localhost:3306/northandsouth,root,rootpass,com.mysql.jdbc.Driver");
-		return properties;
-	}
-	
-		@Override
+	@Invalidate
 	public void stop(BundleContext context) throws Exception {
 			try {
 				log.info("Stopping the bundle"+this.getClass().getName());
-				System.out.println("Stopping the bundle"+this.getClass().getName());
-				GossRequestHandlerRegistrationService registrationService = (GossRequestHandlerRegistrationService) registrationTracker.getService();
-
 				if (registrationService != null) {
 					registrationService.removeHandlerMapping(RequestActualTotalHandler.class);
 					registrationService.removeHandlerMapping(RequestCapacityRequirementHandler.class);
@@ -108,34 +100,9 @@ public class FusionDBServerActivator implements BundleActivator, ManagedService{
 			}
 	}
 
-	@Override
+	@Updated
 	public void updated(Dictionary properties) throws ConfigurationException {
-		// TODO Auto-generated method stub
-		
+		log.info("Updating configuration if required for "+this.getClass().getName());
 	}
 	
-	private void registerRequestAndHandlers(){
-		GossRequestHandlerRegistrationService registrationService = (GossRequestHandlerRegistrationService) registrationTracker.getService();
-		
-		if(registrationService != null){
-			// Registering service handlers here
-			//-------------------------------------Fusion----------------------------------------------
-			registrationService.addHandlerMapping(RequestActualTotal.class, RequestActualTotalHandler.class);
-			registrationService.addHandlerMapping(RequestCapacityRequirement.class, RequestCapacityRequirementHandler.class);
-			registrationService.addHandlerMapping(RequestForecastTotal.class, RequestForecastTotalHandler.class);
-			registrationService.addHandlerMapping(RequestHAInterchangeSchedule.class, RequestHAInterchangeScheduleHandler.class);
-			registrationService.addHandlerMapping(RequestRTEDSchedule.class, RequestRTEDScheduleHandler.class);
-			
-			//-------------------------------------Fusion Security----------------------------------------------
-			registrationService.addSecurityMapping(RequestActualTotal.class, AccessControlHandlerAllowAll.class);
-			registrationService.addSecurityMapping(RequestCapacityRequirement.class, AccessControlHandlerAllowAll.class);
-			registrationService.addSecurityMapping(RequestForecastTotal.class, AccessControlHandlerAllowAll.class);
-			registrationService.addSecurityMapping(RequestHAInterchangeSchedule.class, AccessControlHandlerAllowAll.class);
-			registrationService.addSecurityMapping(RequestRTEDSchedule.class, AccessControlHandlerAllowAll.class);
-		}
-		else{
-			log.debug(GossRequestHandlerRegistrationService.class.getName()+" not found!");
-		}		
-	}
-
 }
