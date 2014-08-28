422688914047e3e7a59c0258c8bcb7689cdbee27
Craig Allwardt <craig.allwardt@pnnl.gov>
2014-08-28 00:58:03 -0700

    An element identifier will uniquely identify a particular type of object.  In addition specifies the datatype of the object for querying.

---
+++ goss-powergrid/powergrid-common/src/main/java/pnnl/goss/nodebreaker/ElementIdentifier.java
@@ -0,0 +1,36 @@
+package pnnl.goss.nodebreaker;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class ElementIdentifier  implements Serializable{
+	
+	private String mrid;
+	
+	private String dataType;
+	
+	public ElementIdentifier(){
+		
+	}
+	
+	public ElementIdentifier(String mrid, String dataType){
+		this.mrid = mrid;
+		this.dataType = dataType;
+	}
+	
+	public String getMrid() {
+		return mrid;
+	}
+	public void setMrid(String mrid) {
+		this.mrid = mrid;
+	}
+	public String  getDataType() {
+		return dataType;
+	}
+	public void setDataType(String dataType) {
+		this.dataType = dataType;
+	}
+}
