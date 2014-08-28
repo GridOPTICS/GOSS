63a2b135a22cae46d2ac15f1b1d76eaa1c09e5c7
Craig Allwardt <craig.allwardt@pnnl.gov>
2014-08-28 01:00:03 -0700

    Added BreakerDao and persistance.xml file to support the persistance layer of the system.

---
+++ goss-powergrid/powergrid-server/src/main/java/pnnl/goss/nodebreaker/dao/BreakerDao.java
@@ -0,0 +1,45 @@
+package pnnl.goss.nodebreaker.dao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import pnnl.goss.nodebreaker.Breaker;
+import pnnl.goss.nodebreaker.ElementIdentifier;
+
+
+public class BreakerDao {
+	EntityManagerFactory emf;
+
+    EntityManager em;
+
+    public BreakerDao(String persistenceUnitName)
+    {
+        if (emf == null)
+        {
+            emf = Persistence.createEntityManagerFactory(persistenceUnitName);
+        }
+    }
+    
+    public Object get(Class<?> entityClass, ElementIdentifier id)
+    {
+        EntityManager em = emf.createEntityManager();
+        Object p = em.find(entityClass, id);
+        return p;
+    }
+    
+    public void persist(Breaker car){
+    	em = emf.createEntityManager();
+    	/*for(Terminal t: car.getTerminals()){
+    		em.persist(t);
+    		
+    	}*/
+    	em.persist(car);
+    	if (em != null)
+        {
+            em.close();
+            em = null;
+        }    	
+    }
+
+}
