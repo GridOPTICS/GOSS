b883fbbeeb6cea4d67e8707e54a8230c7bbb019f
Craig Allwardt <craig.allwardt@pnnl.gov>
2014-09-04 18:49:28 -0700

    Fixed up items so they are saving properly in cassandra.  Now to replicate and finish.

--- goss-powergrid/powergrid-common/src/main/java/pnnl/goss/powergrid/topology/NodeBreakerDataType.java
+++ goss-powergrid/powergrid-common/src/main/java/pnnl/goss/powergrid/topology/NodeBreakerDataType.java
@@ -2,9 +2,22 @@ package pnnl.goss.powergrid.topology;
 
 public interface NodeBreakerDataType {
 	
-	public static final String SUBSTATION = "Substation";
+	public static final String MRID = "MRID";
+	public static final String NAME = "NAME";
+	public static final String ALIAS = "ALIAS";
+	public static final String PATH = "PATH";
+	public static final String DESCRIPTION = "DESCRIPTION";
+	public static final String DATA_TYPE = "DATA_TYPE";
+			
+	public static final String SUBSTATION = "SUBSTATION";
+	public static final String SUBSTATION_MRID = "SUBSTATION_MRID";
 	
-	public static final String VOLTAGE_LEVEL = "VoltageLevel";
+	public static final String VOLTAGE_LEVEL = "VOLTAGE_LEVEL";
+	public static final String VOLTAGE_LEVEL_MRID = "VOLTAGE_LEVEL_MRID";
+	
+	public static final String SYNCHRONOUES_MACHINE = "SYNCHRONOUES_MACHINE";
+	
+	public static final String MEASUREMENT_TYPE = "MEASUREMENT_TYPE";
 	
 	public String getDataType();
 	
