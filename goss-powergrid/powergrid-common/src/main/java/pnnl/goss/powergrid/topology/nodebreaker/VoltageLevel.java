e78afc7bd526907b724d7c340ab10ac52e2e6f86
Craig Allwardt <craig.allwardt@pnnl.gov>
2014-09-04 18:51:06 -0700

    Fixed up items so that they save properly.  Now on to replicate and finish up.

--- goss-powergrid/powergrid-common/src/main/java/pnnl/goss/powergrid/topology/nodebreaker/VoltageLevel.java
+++ goss-powergrid/powergrid-common/src/main/java/pnnl/goss/powergrid/topology/nodebreaker/VoltageLevel.java
@@ -1,11 +1,13 @@
 package pnnl.goss.powergrid.topology.nodebreaker;
 
+import static pnnl.goss.powergrid.topology.NodeBreakerDataType.DATA_TYPE;
+import static pnnl.goss.powergrid.topology.NodeBreakerDataType.SUBSTATION;
 import static pnnl.goss.powergrid.topology.NodeBreakerDataType.VOLTAGE_LEVEL;
+import static pnnl.goss.powergrid.topology.NodeBreakerDataType.VOLTAGE_LEVEL_MRID;
 
+import javax.persistence.AttributeOverride;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import pnnl.goss.powergrid.topology.IdentifiedObject;
@@ -16,14 +18,16 @@ import com.impetus.kundera.index.IndexCollection;
 
 @Entity
 @Table(name=VOLTAGE_LEVEL)
-@IndexCollection(columns={@Index(name="dataType")})
+@AttributeOverride(name="mrid", column=@Column(name=VOLTAGE_LEVEL_MRID))
+@IndexCollection(columns={@Index(name=DATA_TYPE)})
 public class VoltageLevel extends IdentifiedObject {
 	
-	@Column
+	private static final long serialVersionUID = -3855948715546769934L;
+
+	@Column(name=DATA_TYPE)
 	protected String dataType;
 	
-	@ManyToOne
-	@JoinColumn(name="mrid")
+	@Column(name=SUBSTATION)
 	protected Substation substation;
 	
 	public VoltageLevel(){
