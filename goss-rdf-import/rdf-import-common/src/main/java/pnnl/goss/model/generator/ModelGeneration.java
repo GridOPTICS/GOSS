aff0c15a450589464e063546d4ce3b2c2f708d6d
d3m614 <d3m614@d3m614-Latitude-E6510>
2014-09-12 23:51:28 -0700

    Model generator can now create packages.

--- goss-rdf-import/rdf-import-common/src/main/java/pnnl/goss/model/generator/ModelGeneration.java
+++ goss-rdf-import/rdf-import-common/src/main/java/pnnl/goss/model/generator/ModelGeneration.java
@@ -3,6 +3,9 @@ package pnnl.goss.model.generator;
 import java.io.File;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Map;
 
 public class ModelGeneration {
 	
@@ -15,6 +18,41 @@ public class ModelGeneration {
 	 * The root of the java source folder.
 	 */
 	private static final String ROOT_FOLDER = "src/main/java";
+	
+	/**
+	 * Creates a package directory under ROOT_FOLDER.
+	 * 
+	 * If the folder already exists then it is not created.
+	 * 
+	 * @param classPackage A package that shoudl be created.
+	 */
+	public static void createPackageDir(String classPackage){
+		System.out.println("Creating package: "+classPackage);
+		String packageDir = classPackage.replace(".", "/");
+		Path dir = Paths.get(ROOT_FOLDER, packageDir);
+
+		// Already exists
+		if (dir.toFile().isDirectory()){
+			return;
+		}
+		
+		if (!dir.toFile().mkdirs()){
+			System.out.println("Failed to create: "+dir.toAbsolutePath());
+		}
+	}
+	
+	/**
+	 * Generates a class
+	 * 
+	 * @param classPackage the package the generated class will belong to.
+	 * @param className the name of the class to be generated.
+	 * @param extendsClass a baseclass if necessary (null or "" if not)
+	 * @param attributeTypeMap a Map of attribute names -> datatype (full package notation).
+	 */
+	public static void createClassFile(String classPackage, String className,
+			String extendsClass, Map<String, String> attributeTypeMap){
+		
+	}
 
 	/**
 	 * Starts the generation of the models that are in the xls file
@@ -23,7 +61,7 @@ public class ModelGeneration {
 	 */
 	public static void generateModels(File existingFile){
 		System.out.println("Generating models ...");
-		
+		createPackageDir("pnnl.goss.cim");
 		System.out.println("Generation complete");
 	}
 	
