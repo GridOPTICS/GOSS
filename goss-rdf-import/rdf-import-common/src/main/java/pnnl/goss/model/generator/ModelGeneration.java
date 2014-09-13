73d507b93d9465a487555c07f418c1f6af068d0d
d3m614 <d3m614@d3m614-Latitude-E6510>
2014-09-12 23:26:11 -0700

    Added boilerplate function to create models.

--- goss-rdf-import/rdf-import-common/src/main/java/pnnl/goss/model/generator/ModelGeneration.java
+++ goss-rdf-import/rdf-import-common/src/main/java/pnnl/goss/model/generator/ModelGeneration.java
@@ -1,16 +1,40 @@
 package pnnl.goss.model.generator;
 
-import java.net.URI;
+import java.io.File;
+import java.net.URISyntaxException;
 import java.net.URL;
 
 public class ModelGeneration {
-
 	
-	public static void main(String[] args) {
+	/**
+	 * All generated classes will be under this root.
+	 */
+	private static final String ROOT_PACKAGE = "pnnl.goss.cim";
+	
+	/**
+	 * The root of the java source folder.
+	 */
+	private static final String ROOT_FOLDER = "src/main/java";
+
+	/**
+	 * Starts the generation of the models that are in the xls file
+	 * 
+	 * @param existingFile The downloaded xls file from ERCOT.
+	 */
+	public static void generateModels(File existingFile){
+		System.out.println("Generating models ...");
+		
+		System.out.println("Generation complete");
+	}
+	
+	public static void main(String[] args) throws URISyntaxException {
 		
 		// Root of the resources path at src/main/resources
-		URL uri = ModelGeneration.class.getClassLoader().getResource("ERCOT_DataDictionary_1.7.xls");
-		System.out.println(uri.toString());
+		URL url = ModelGeneration.class.getClassLoader().getResource("ERCOT_DataDictionary_1.7.xls");
+		File file = new File(url.toURI());
+		if (file.exists()){
+			generateModels(file);
+		}
 
 	}
 
