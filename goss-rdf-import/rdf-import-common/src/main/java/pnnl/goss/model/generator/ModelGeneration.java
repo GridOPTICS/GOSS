ef4a46356ec23f845c8fe9d5223e36066bf61d77
d3m614 <d3m614@d3m614-Latitude-E6510>
2014-09-13 00:05:30 -0700

    Added reading of xls to generateModels.

--- goss-rdf-import/rdf-import-common/src/main/java/pnnl/goss/model/generator/ModelGeneration.java
+++ goss-rdf-import/rdf-import-common/src/main/java/pnnl/goss/model/generator/ModelGeneration.java
@@ -1,11 +1,18 @@
 package pnnl.goss.model.generator;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Map;
+
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 
 public class ModelGeneration {
 	
@@ -58,14 +65,72 @@ public class ModelGeneration {
 	 * Starts the generation of the models that are in the xls file
 	 * 
 	 * @param existingFile The downloaded xls file from ERCOT.
+	 * @throws IOException 
 	 */
-	public static void generateModels(File existingFile){
+	public static void generateModels(File existingFile) throws IOException{
 		System.out.println("Generating models ...");
+		HSSFWorkbook wb = readFile(existingFile);
+
+		System.out.println("Data dump:\n");
+
+		for (int k = 0; k < wb.getNumberOfSheets(); k++) {
+			HSSFSheet sheet = wb.getSheetAt(k);
+			int rows = sheet.getPhysicalNumberOfRows();
+			System.out.println("Sheet " + k + " \"" + wb.getSheetName(k) + "\" has " + rows
+					+ " row(s).");
+			for (int r = 0; r < rows; r++) {
+				HSSFRow row = sheet.getRow(r);
+				if (row == null) {
+					continue;
+				}
+
+				int cells = row.getPhysicalNumberOfCells();
+				System.out.println("\nROW " + row.getRowNum() + " has " + cells
+						+ " cell(s).");
+				for (int c = 0; c < cells; c++) {
+					HSSFCell cell = row.getCell(c);
+					String value = null;
+					
+					if(cell == null){
+						System.out.println("CELL col (c)=" + c + " is null!");
+						continue;
+					}
+
+					switch (cell.getCellType()) {
+
+						case HSSFCell.CELL_TYPE_FORMULA:
+							value = "FORMULA value=" + cell.getCellFormula();
+							break;
+
+						case HSSFCell.CELL_TYPE_NUMERIC:
+							value = "NUMERIC value=" + cell.getNumericCellValue();
+							break;
+
+						case HSSFCell.CELL_TYPE_STRING:
+							value = "STRING value=" + cell.getStringCellValue();
+							break;
+
+						default:
+					}
+					
+					System.out.println("CELL col=" + cell.getColumnIndex() + " VALUE="
+								+ value);
+					
+				}
+			}
+		}
 		createPackageDir("pnnl.goss.cim");
 		System.out.println("Generation complete");
 	}
 	
-	public static void main(String[] args) throws URISyntaxException {
+	/**
+	 * creates an {@link HSSFWorkbook} the specified OS filename.
+	 */
+	private static HSSFWorkbook readFile(File xlsFile) throws IOException {
+		return new HSSFWorkbook(new FileInputStream(xlsFile));
+	}
+	
+	public static void main(String[] args) throws URISyntaxException, IOException {
 		
 		// Root of the resources path at src/main/resources
 		URL url = ModelGeneration.class.getClassLoader().getResource("ERCOT_DataDictionary_1.7.xls");
