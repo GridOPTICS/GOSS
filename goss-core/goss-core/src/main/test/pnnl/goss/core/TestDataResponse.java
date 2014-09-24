fd73787c90c0975d9a93200ec2a10283933b779b
Craig Allwardt <craig.allwardt@pnnl.gov>
2014-09-23 19:23:24 -0700

    Added dataresponse tests.

---
+++ goss-core/goss-core/src/main/test/pnnl/goss/core/TestDataResponse.java
@@ -0,0 +1,41 @@
+package pnnl.goss.core;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestDataResponse {
+	
+	private DataResponse dataResponse;
+	
+	@Before
+	public void setup(){
+		dataResponse = new DataResponse();
+	}
+	
+	@Test
+	public void canSetAndGetData(){
+		String originalData = "This is data";
+		dataResponse.setData(originalData);
+		Assert.assertEquals(originalData,  dataResponse.getData(), "Data wasn't equal");
+	}
+	
+	@Test
+	public void canSetDataThroughConstructor(){
+		String originalData = "Hey original data";
+		dataResponse = new DataResponse(originalData);
+		Assert.assertEquals(originalData,  dataResponse.getData(), "Data wasn't equal through constructor");
+	}
+	
+	@Test
+	public void responseCompleteIsFalseByDefault(){
+		Assert.assertFalse(dataResponse.isResponseComplete());
+	}
+	
+	@Test
+	public void responseCompleeteCanChange(){
+		dataResponse.setResponseComplete(true);
+		Assert.assertTrue(dataResponse.isResponseComplete());
+	}
+
+}
