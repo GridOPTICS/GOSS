a1f424a655693a00f5483d24ec72cf9d1fcfd7c3
d3m614 <craig.allwardt@pnnl.gov>
2014-09-28 13:25:26 -0700

    Internalized the parsing of the xl file to build the meta definitions.

--- goss-rdf-import/rdf-import-common/src/main/java/pnnl/goss/model/generator/MetaClass.java
+++ goss-rdf-import/rdf-import-common/src/main/java/pnnl/goss/model/generator/MetaClass.java
@@ -5,21 +5,51 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+
 public class MetaClass {
+	
+	// Classes sheet
+	private static final Integer CLASSES_PACKAGE_COLUMN = 0;
+	private static final Integer CLASSES_CLASS_COLUMN = 1;
+	private static final Integer CLASSES_INHERITANCE_COLUMN = 3;
+	private static final Integer CLASSES_NAMESPACE_COLUMN = 4;
+	private static final Integer CLASSES_DOCUMENTATION_COLUMN = 5;
 	
 	private List<MetaAttribute> attributes = new ArrayList<>();
 	private String className;
 	private String packageName;
 	private String extendsType;
 	private String classDocumentation;
+	private String packageAndClass;
+	private String inheritancePath;
 	
+	private static String packageRoot;
+	
+	public static void setStaticPackageRoot(String staticRoot){
+		packageRoot = staticRoot;
+	}
+	
+	public static String getStaticPackageRoot(){
+		return packageRoot;
+	}
+	
+	public String getInheritancePath() {
+		return inheritancePath;
+	}
+
+	public void setInheritancePath(String inheritancePath) {
+		this.inheritancePath = inheritancePath;
+	}
+
 	public boolean isValidClassDefinition(){
 		return className != null && className.length() > 0 &&
 				packageName != null && packageName.length() > 0;
 	}
 	
-	public String getDataType(){
-		return packageName + "." + className;
+	public String getPackageAndClass(){
+		return packageAndClass;
 	}
 	
 	public String getExtendsType() {
@@ -160,6 +190,105 @@ public class MetaClass {
 			buf.append(" Extends: " + extendsType);
 		}
 		return buf.toString();
+	}
+	
+	public static MetaClass create(HSSFRow row){
 		
+		MetaClass cls = new MetaClass();
+		
+		if (row == null) {
+			return null;
+		}
+		//HSSFCell pkgCell = row.getCell(CLASSES_PACKAGE_COLUMN);
+		HSSFCell pkgCell = row.getCell(CLASSES_NAMESPACE_COLUMN);
+		if (pkgCell != null){
+			if (pkgCell.getCellType() == HSSFCell.CELL_TYPE_NUMERIC){
+				System.out.println("Skipping: " +pkgCell.getNumericCellValue());
+			}
+			else if(pkgCell.getCellType() == HSSFCell.CELL_TYPE_STRING){
+				cls.setPackageName(getPackage(pkgCell.getStringCellValue()));
+				cls.setClassName(row.getCell(CLASSES_CLASS_COLUMN).getStringCellValue());
+				
+				if (!cls.isValidClassDefinition()){
+					return null;
+				}
+				
+				cls.setInheritancePath(row.getCell(CLASSES_INHERITANCE_COLUMN).getStringCellValue());
+				cls.packageAndClass = cls.getPackageName() + "." + cls.getClassName();
+				cls.classDocumentation = row.getCell(CLASSES_DOCUMENTATION_COLUMN).getStringCellValue();			
+			}
+//				// Use this as a test so that we can get the mapped type and see
+//				// if it's already in the collection (Hopefully it never will!)
+//				MetaClass tmp = new MetaClass();
+//				tmp.setPackageName(getPackage(pkgCell.getStringCellValue()));
+//				tmp.setClassName(row.getCell(CLASSES_CLASS_COLUMN).getStringCellValue());
+//				
+//				if (!tmp.isValidClassDefinition()){
+//					System.out.println("Invalid class detected for row: "+ r);
+//					continue;
+//				}
+//				
+//				// Store the metaclass in our hashmap of classes if necessary.
+//				MetaClass newMetaClass = metaClasses.get(tmp.getDataType());
+//				if (newMetaClass == null){
+//					newMetaClass = tmp;
+//					metaClasses.put(newMetaClass.getDataType(), newMetaClass);
+//					classNameToType.put(newMetaClass.getClassName(), newMetaClass.getDataType());
+//				}
+//				else{
+//					System.out.println("Boo already has the datatype!! "+newMetaClass.getDataType());
+//				}
+//				
+//				HSSFCell docCell = row.getCell(CLASSES_DOCUMENTATION_COLUMN);
+//				if (docCell != null && docCell.getStringCellValue() != null){
+//					newMetaClass.setClassDocumentation(docCell.getStringCellValue());
+//				}
+//			}
+		}
+		return cls;
+	}
+		
+	/**
+	 * Gets the string of the package that a class should be in.  The package is expected
+	 * to be from the first column of the Classes worksheet in the xls file.
+	 * 
+	 * @param data Data in the cell of the first column of the classes tab in the xls file.
+	 * @return The package or null if not available.
+	 */
+	private static String getPackage(String data){
+		String clspackage = ModelGeneration.ROOT_PACKAGE;
+		
+		// This used the package data from the first column to generate the package
+		if (data.length() > 3){
+			if(data.startsWith("ETXSCADA")){
+				clspackage += ".ext.scada";
+			}
+			else if(data.startsWith("CIMSCADA")){
+				clspackage += ".scada";
+			}
+			else if(data.startsWith("CIM") || data.startsWith("EXT")){
+				
+				if (data.startsWith("EXT")){
+					clspackage += "ext";
+				}
+				
+				for(int i=3; i < data.length();i++){
+					if(Character.isUpperCase(data.charAt(i))){
+						clspackage += "."+Character.toString(Character.toLowerCase(data.charAt(i)));
+					}
+					else{
+						clspackage += data.charAt(i);
+					}
+				}
+			}
+			else{
+				clspackage = null;
+			}
+		}
+		else{
+			clspackage += "." + data.toLowerCase();
+		}
+		
+		return clspackage;
 	}
 }
--- goss-rdf-import/rdf-import-common/src/main/java/pnnl/goss/model/generator/MetaDataType.java
+++ goss-rdf-import/rdf-import-common/src/main/java/pnnl/goss/model/generator/MetaDataType.java
@@ -3,6 +3,11 @@ package pnnl.goss.model.generator;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+
+import pnnl.goss.model.generator.ModelGeneration.DataTypeSheets;
+
 public class MetaDataType {
 	
 	private boolean isEnumeration;
@@ -17,8 +22,35 @@ public class MetaDataType {
 	private String documentation;
 	private Set<String> enumeratedValues = new HashSet<>();
 	
+	// Attributes sheet
+	private static final Integer ATTRIB_CLASS_COLUMN = 2;
+	private static final Integer ATTRIB_ATTRIBUTE_COLUMN = 4;
+	private static final Integer ATTRIB_DATA_TYPE_COLUMN = 5;
+	private static final Integer ATTRIB_INITIAL_VALUE_COLUMN = 6;
+	private static final Integer ATTRIB_DOCUMENTATION_COLUMN = 8;
+	// DataType sheet
+	private static final Integer DATATYPE_PACKAGE_COLUMN = 0;
+	private static final Integer DATATYPE_DATA_TYPE_CoLUMN = 1;
+	private static final Integer DATATYPE_NS_CoLUMN = 2;
+	private static final Integer DATATYPE_DOCUMENTATION_CoLUMN = 3;
+	// DataType Value & Unit sheet
+	private static final Integer DATATYPEVALUE_PACKAGE_CoLUMN = 0;
+	private static final Integer DATATYPEVALUE_DATA_TYPE_CoLUMN = 2;
+	private static final Integer DATATYPEVALUE_DATA_TYPE_NAME_CoLUMN = 1;
+	private static final Integer DATATYPEVALUE_NS_CoLUMN = 4;
+	// DataType - Enum sheet
+	private static final Integer DATATYPEENUM_PACKAGE_COLUMN = 0;
+	private static final Integer DATATYPEENUM_ENUM_TYPE_COLUMN = 1;
+	private static final Integer DATATYPEENUM_ENUM_VALUE_COLUMN = 2;
+	private static final Integer DATATYPEENUM_DOCUMENTATION_CoLUMN = 3;
+	private static final Integer DATATYPEENUM_NS_CoLUMN = 4;
+	
 	public MetaDataType(){
 		
+	}
+	
+	private static String getEnumerationPackage(){
+		return ModelGeneration.ROOT_PACKAGE.concat(".enumerations");
 	}
 	
 	public MetaDataType(String dataTypeName){
@@ -30,7 +62,7 @@ public class MetaDataType {
 	}
 	
 	public MetaDataType(MetaClass metaClass){
-		this.dataTypeName = metaClass.getDataType();
+		this.dataTypeName = metaClass.getPackageAndClass();
 		this.namespace = metaClass.getPackageName()
 				.substring(metaClass.getPackageName()
 						.lastIndexOf("."));
@@ -214,6 +246,92 @@ public class MetaDataType {
 		
 		return buf.toString();
 		
+	}
+	
+	public static MetaDataType create(HSSFRow row, DataTypeSheets sheetType){
+		
+		if (row == null) {
+			return null;
+		}
+		
+		MetaDataType cls = new MetaDataType();
+		
+		HSSFCell packageCell= null;
+		switch (sheetType){
+		case DataTypeAndUnits:
+			packageCell = row.getCell(DATATYPEVALUE_PACKAGE_CoLUMN); 
+			if (packageCell != null && packageCell.getStringCellValue()!= null){
+				
+				HSSFCell namespaceCell = row.getCell(DATATYPEVALUE_NS_CoLUMN);
+				HSSFCell dataTypeNameCell = row.getCell(DATATYPEVALUE_DATA_TYPE_NAME_CoLUMN);
+				HSSFCell dataTypeCell = row.getCell(DATATYPEVALUE_DATA_TYPE_CoLUMN);
+				String dataTypeName = dataTypeNameCell.getStringCellValue();
+				
+				String namespace = namespaceCell.getStringCellValue();
+				
+				cls.setDataTypeName(dataTypeName);
+				cls.setNamespace(namespace);
+				
+				if (dataTypeCell != null){
+					String dataTypeValue = dataTypeCell.getStringCellValue();
+					cls.setValueType(dataTypeValue);
+				}				
+			}
+			break;
+		case DataType:
+			packageCell = row.getCell(DATATYPE_PACKAGE_COLUMN); 
+			if (packageCell != null && packageCell.getStringCellValue()!= null){
+				boolean isEnum = packageCell.getStringCellValue().contains("Enum");
+				
+				HSSFCell namespaceCell = row.getCell(DATATYPE_NS_CoLUMN);
+				HSSFCell dataTypeCell = row.getCell(DATATYPE_DATA_TYPE_CoLUMN); 
+				String dataTypeName = dataTypeCell.getStringCellValue();
+				String namespace = namespaceCell.getStringCellValue();
+				
+				cls.setDataTypeName(dataTypeName);
+				cls.setNamespace(namespace);
+				cls.setEnumeration(isEnum);
+				
+
+				// metaDataType.put(meta.getDataTypeName(), meta);
+			}
+			break;
+		case DataTypeEnums:
+			packageCell = row.getCell(DATATYPEENUM_PACKAGE_COLUMN); 
+			if (packageCell != null && packageCell.getStringCellValue()!= null){
+									
+				HSSFCell namespaceCell = row.getCell(DATATYPEENUM_NS_CoLUMN);
+				HSSFCell enumTypeCell = row.getCell(DATATYPEENUM_ENUM_TYPE_COLUMN);
+				HSSFCell enumValueCell = row.getCell(DATATYPEENUM_ENUM_VALUE_COLUMN);
+				
+				String enumTypeName = enumTypeCell.getStringCellValue();
+				String enumValue = enumValueCell.getStringCellValue();
+				String namespace = namespaceCell.getStringCellValue();
+				
+				cls.setDataTypeName(enumTypeName);
+				cls.setNamespace(namespace);
+				cls.setEnumeration(true);
+				cls.setJavaPackage(getEnumerationPackage());
+				cls.addEnumeratedValue(enumValue);
+//				MetaDataType meta = null; 
+//				// Handle the addition of enumeration values
+//				if(metaDataType.containsKey(enumTypeName)){
+//					meta = metaDataType.get(enumTypeName);
+//					meta.addEnumeratedValue(enumValue);
+//					meta.setJavaPackage(getEnumerationPackage());
+//					meta.setEnumeration(true);
+//				}
+//				else{
+//					meta = new MetaDataType();
+//					
+//					// metaDataType.put(meta.getDataTypeName(), meta);
+//				}
+			}
+			break;
+		}	
+	
+		
+		return cls;
 	}
 	
 }
