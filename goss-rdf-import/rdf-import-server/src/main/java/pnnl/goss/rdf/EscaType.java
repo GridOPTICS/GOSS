527bb1b5c0299c4805e42c0d9875b5bb20ef19de
Craig Allwardt <craig.allwardt@pnnl.gov>
2014-10-01 15:04:25 -0700

    Removed the concept of children from the escatree and escatype model

--- goss-rdf-import/rdf-import-server/src/main/java/pnnl/goss/rdf/EscaType.java
+++ goss-rdf-import/rdf-import-server/src/main/java/pnnl/goss/rdf/EscaType.java
@@ -21,12 +21,11 @@ public class EscaType {
 	private Resource resource;
 	private String dataType;
 	private String mrid;
-	private Set<EscaType> children = new HashSet<EscaType>();
-	private EscaType parent = null;
 	private static Logger log = LoggerFactory.getLogger(EscaType.class);
 	
 	// Property name-> Literal value (i.e. String, Integer, Float etc)
 	private Map<String, Literal> literals = new HashMap<>();
+
 	// Property name->esca type
 	private Map<String, EscaType> links = new HashMap<>();
 	
@@ -79,36 +78,6 @@ public class EscaType {
 	public Map<String, Literal> getLiterals(){
 		return literals;
 	}
-			
-	public int getLevel(){
-		if (parent == null){
-			return 0;
-		}
-		else{
-			return parent.getLevel() + 1;
-		}
-	}
-	
-	public EscaType getParent(){
-		return parent;
-	}
-	
-	public void setParent(EscaType parent){
-		System.out.println(this.mrid + " parent set to " + parent.mrid);
-		this.parent = parent;
-	}	
-	
-	public boolean hasChildren(){
-		return this.children.size() > 0;
-	}
-	
-	public void clearChildren(){
-		this.children.clear();
-	}
-	
-	public void removeChild(EscaType child){
-		this.children.remove(child);
-	}
 	
 	/**
 	 * @return the resource
@@ -140,33 +109,7 @@ public class EscaType {
 		this.dataType = dataType;
 		this.mrid = mrid;
 	}
-	
-	public void addChild(EscaType link){
-		children.add(link);
-	}
-	
-	public Collection<EscaType> getChildren(){
-		return children;
-	}
-	
-	public String toString(int level){
-		StmtIterator itr = resource.listProperties();
-		StringBuilder sb = new StringBuilder();
-		String tabs = repeat("\t", level);
-		sb.append(tabs + this.dataType + " ("+this.mrid+")\n");
-		// Add one tab for the literals.
-		tabs = tabs + "\t";
-		
-		while(itr.hasNext()){
-			Statement stmt = itr.nextStatement();
-			RDFNode node = stmt.getObject();
-			Property prop = stmt.getPredicate();
-			if (node.isLiteral()){
-				sb.append(tabs + prop.getLocalName()+" = "+node.asLiteral()+"\n");
-			}			
-		}
-		return sb.toString();
-	}
+
 	
 	@Override
 	public String toString() {
