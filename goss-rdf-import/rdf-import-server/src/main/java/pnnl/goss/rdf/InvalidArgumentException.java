6342ef4986484858b932da003bc533bb1ef7319a
d3m614 <craig.allwardt@pnnl.gov>
2014-10-01 18:46:42 -0700

    Added Exception class for invalid arguments

---
+++ goss-rdf-import/rdf-import-server/src/main/java/pnnl/goss/rdf/InvalidArgumentException.java
@@ -0,0 +1,11 @@
+package pnnl.goss.rdf;
+
+public class InvalidArgumentException extends Exception {
+
+	private static final long serialVersionUID = -5379484972083760055L;
+	
+	public InvalidArgumentException(String message) {
+		super(message);	
+	}
+
+}
