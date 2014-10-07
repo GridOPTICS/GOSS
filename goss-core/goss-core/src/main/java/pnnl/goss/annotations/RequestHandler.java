1e6bed96ab087d86897ab2b23ed55f62ec7a0827
Craig Allwardt <craig.allwardt@pnnl.gov>
2014-10-07 13:32:39 -0700

    Added annotation for adding request handlers.

---
+++ goss-core/goss-core/src/main/java/pnnl/goss/annotations/RequestHandler.java
@@ -0,0 +1,18 @@
+package pnnl.goss.annotations;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import pnnl.goss.core.Request;
+
+@Documented
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface RequestHandler {
+	
+	Class<? extends Request>[] requests();
+
+}
