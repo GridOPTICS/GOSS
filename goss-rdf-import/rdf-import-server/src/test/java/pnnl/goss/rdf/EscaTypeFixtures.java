76fa50dde95e0345a96879a68921c33291503b4b
Craig Allwardt <craig.allwardt@pnnl.gov>
2014-10-10 19:39:57 -0700

    Adding fixtures.

--- goss-rdf-import/rdf-import-server/src/test/java/pnnl/goss/rdf/EscaTypeFixtures.java
+++ goss-rdf-import/rdf-import-server/src/test/java/pnnl/goss/rdf/EscaTypeFixtures.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 import pnnl.goss.rdf.impl.EscaTypeImpl;
 import pnnl.goss.rdf.impl.EscaTypes;
+import pnnl.goss.rdf.server.Esca60Vocab;
 
 import com.hp.hpl.jena.rdf.model.Resource;
 
@@ -44,6 +45,12 @@ public class EscaTypeFixtures {
 			type.addDirectLink(property, types.get(toMrids[i]));
 		}
 	}
+	
+	private static void addDirectLink(EscaTypes types, String mridFrom, String mridTo, String propertyName){
+		EscaType fromEsca = types.get(mridFrom);
+		EscaType toEsca = types.get(mridTo);
+		fromEsca.addDirectLink(propertyName, toEsca);
+	}
 
 	/**
 	 * Describe the nodes that are getting sent back.
@@ -51,9 +58,14 @@ public class EscaTypeFixtures {
 	 */
 	public static EscaTypes getEsca6Node(){
 		
-		String[] terminalMrids = {"_597196580683024629_t",
-				"_5986050453675674768", "_8231210467179973538", "_4963536783062858854"};
-		String[] connectivityMrids = {"_3705119779910468614"};
+		String[] terminals = {"_597196580683024629_t",
+									"_5986050453675674768", 
+									"_8231210467179973538", 
+									"_4963536783062858854"};
+		
+		String[] breakers = {"_1278064856843146094", "_8425939105332198299"};
+		String[] connectivityNodes = {"_3705119779910468614"};
+		String[] voltageLevels={"_5279025887447170952"};
 		
 		EscaTypes types = new EscaTypes();
 		// Terminal Resource
@@ -61,15 +73,29 @@ public class EscaTypeFixtures {
 		when(tRes.getLocalName()).thenReturn("Terminal");
 		// Breaker Resource
 		Resource bRes = mock(Resource.class);
-		when(tRes.getLocalName()).thenReturn("Breaker");
+		when(bRes.getLocalName()).thenReturn("Breaker");
 		// ConnectivityNode Resource
 		Resource cnRes = mock(Resource.class);
-		when(tRes.getLocalName()).thenReturn("ConnectivityNode");
+		when(cnRes.getLocalName()).thenReturn(Esca60Vocab.TERMINAL_CONNECTIVITYNODE.getLocalName());
+		// VoltageLevel Resource
+		Resource vlRes = mock(Resource.class);
+		when(vlRes.getLocalName()).thenReturn("VoltageLevel");
 		
-		addToEscaTypes(types, createEscaTypes(tRes, terminalMrids));
-		addToEscaTypes(types, createEscaTypes(cnRes, connectivityMrids));
+		// Link the terminals to the connectivity node directly
+		addDirectLink(types, "_597196580683024629_t", "_3705119779910468614", Esca60Vocab.TERMINAL_CONNECTIVITYNODE.getLocalName());
+		addDirectLink(types, "_5986050453675674768",  "_3705119779910468614", Esca60Vocab.TERMINAL_CONNECTIVITYNODE.getLocalName());
+		addDirectLink(types, "_8231210467179973538",  "_3705119779910468614", Esca60Vocab.TERMINAL_CONNECTIVITYNODE.getLocalName());
+		addDirectLink(types, "_4963536783062858854",  "_3705119779910468614", Esca60Vocab.TERMINAL_CONNECTIVITYNODE.getLocalName());
 		
-		addDirectLinks(types, "terminal", "_3705119779910468614", terminalMrids);
+		// Link from terminal to the breaker
+		addDirectLink(types, "_5986050453675674768", "_8425939105332198299", Esca60Vocab.TERMINAL_CONDUCTINGEQUIPMENT.getLocalName());
+				
+		
+		addToEscaTypes(types, createEscaTypes(tRes, terminals));
+		addToEscaTypes(types, createEscaTypes(cnRes, connectivityNodes));
+		addToEscaTypes(types, createEscaTypes(vlRes, voltageLevels));
+		addToEscaTypes(types, createEscaTypes(bRes, breakers));
+		
 		
 		
 				
