7b107fddd95e438dd1266c506b246390807637ad
Craig Allwardt <craig.allwardt@pnnl.gov>
2014-10-24 15:58:09 -0700

    Added terminals set to the repository.

---
+++ goss-rdf-import/rdf-import-server/src/main/java/pnnl/goss/rdf/impl/Terminals.java
@@ -0,0 +1,9 @@
+package pnnl.goss.rdf.impl;
+
+import java.util.HashSet;
+
+public class Terminals extends HashSet<Terminal>{
+
+	private static final long serialVersionUID = -5690093079863200972L;
+
+}
