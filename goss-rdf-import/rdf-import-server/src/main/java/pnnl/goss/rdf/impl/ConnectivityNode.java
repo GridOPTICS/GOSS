c11376e79bf5cc14a32f195756b9a736716c3aac
Craig Allwardt <craig.allwardt@pnnl.gov>
2014-10-24 15:33:01 -0700

    Added connectivitynode and connectivity nodes

---
+++ goss-rdf-import/rdf-import-server/src/main/java/pnnl/goss/rdf/impl/ConnectivityNode.java
@@ -0,0 +1,5 @@
+package pnnl.goss.rdf.impl;
+
+public class ConnectivityNode extends AbstractEscaType {
+	
+}
---
+++ goss-rdf-import/rdf-import-server/src/main/java/pnnl/goss/rdf/impl/ConnectivityNodes.java
@@ -0,0 +1,9 @@
+package pnnl.goss.rdf.impl;
+
+import java.util.HashSet;
+
+public class ConnectivityNodes extends HashSet<ConnectivityNode> {
+
+	private static final long serialVersionUID = 4679801416025585916L;
+
+}
