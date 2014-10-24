31ac883dfa0408553e8baa86dbd6693eef805b36
Craig Allwardt <craig.allwardt@pnnl.gov>
2014-10-24 16:03:04 -0700

    Added connection between terminals and connectivity nodes.

--- goss-rdf-import/rdf-import-server/src/main/java/pnnl/goss/rdf/impl/ConnectivityNode.java
+++ goss-rdf-import/rdf-import-server/src/main/java/pnnl/goss/rdf/impl/ConnectivityNode.java
@@ -1,5 +1,25 @@
 package pnnl.goss.rdf.impl;
 
+import pnnl.goss.rdf.EscaType;
+import pnnl.goss.rdf.server.Esca60Vocab;
+
 public class ConnectivityNode extends AbstractEscaType {
+
+	private Terminals terminals;
 	
+	/**
+	 * Lazy load terminals that are connected to this node.
+	 * 
+	 * @return set of terminals connected to this node.
+	 */
+	public Terminals getTerminals(){
+		if(terminals == null){
+			terminals = new Terminals();
+			for(EscaType t: this.getRefersToMe(Esca60Vocab.TERMINAL_OBJECT)) {
+				terminals.add((Terminal)t);
+			}
+		}
+		
+		return terminals;
+	}
 }
