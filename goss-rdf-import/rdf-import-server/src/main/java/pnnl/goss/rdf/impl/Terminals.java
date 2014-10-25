b77863f832ffe3a8d1f4f0eb5240547231069be9
d3m614 <craig.allwardt@pnnl.gov>
2014-10-25 00:32:18 -0700

    Added convinencce method for converting back to base EscaType element.

--- goss-rdf-import/rdf-import-server/src/main/java/pnnl/goss/rdf/impl/Terminals.java
+++ goss-rdf-import/rdf-import-server/src/main/java/pnnl/goss/rdf/impl/Terminals.java
@@ -1,9 +1,22 @@
 package pnnl.goss.rdf.impl;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashSet;
+import java.util.List;
+
+import pnnl.goss.rdf.EscaType;
 
 public class Terminals extends HashSet<Terminal>{
 
 	private static final long serialVersionUID = -5690093079863200972L;
+	
+	public Collection<EscaType> toEscaTypeCollection(){
+		List<EscaType> types = new ArrayList<>();
+		for(Terminal t: this){
+			types.add(t);
+		}
+		return types;
+	}
 
 }
