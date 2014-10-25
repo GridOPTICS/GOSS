ae3d36e538ddfae2c99311616b6488d566453552
d3m614 <craig.allwardt@pnnl.gov>
2014-10-25 01:24:23 -0700

    Added a toEscaTypes Collection.

--- goss-rdf-import/rdf-import-server/src/main/java/pnnl/goss/rdf/impl/ConnectivityNodes.java
+++ goss-rdf-import/rdf-import-server/src/main/java/pnnl/goss/rdf/impl/ConnectivityNodes.java
@@ -1,9 +1,22 @@
 package pnnl.goss.rdf.impl;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashSet;
+import java.util.List;
+
+import pnnl.goss.rdf.EscaType;
 
 public class ConnectivityNodes extends HashSet<ConnectivityNode> {
 
 	private static final long serialVersionUID = 4679801416025585916L;
+	
+	public Collection<EscaType> toEscaTypeCollection(){
+		List<EscaType> types = new ArrayList<>();
+		for(ConnectivityNode t: this){
+			types.add(t);
+		}
+		return types;
+	}
 
 }
