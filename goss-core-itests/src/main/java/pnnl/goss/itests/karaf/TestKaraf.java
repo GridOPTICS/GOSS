1fbe78317d5dc0568dd5fa69f233c1aa9ec1486d
Craig Allwardt <craig.allwardt@pnnl.gov>
2014-11-20 21:29:01 -0800

    working on getting data to work properly.

--- goss-core-itests/src/main/java/pnnl/goss/itests/karaf/TestKaraf.java
+++ goss-core-itests/src/main/java/pnnl/goss/itests/karaf/TestKaraf.java
@@ -28,52 +28,71 @@ import pnnl.goss.core.server.GossRequestHandlerRegistrationService;
 @RunWith(PaxExam.class)
 public class TestKaraf {
 	private static Logger LOG = LoggerFactory.getLogger(TestKaraf.class);
-	
-	@Inject
-	protected GossRequestHandlerRegistrationService registrationService;
-	
-	@Configuration
-	public Option[] config() {
-		MavenArtifactUrlReference karafUrl = maven()
-				.groupId("org.apache.karaf")
-				.artifactId("apache-karaf")
-				.version(karafVersion())
-				.type("zip");
-		MavenUrlReference karafStandardRepo = maven()
-				.groupId("org.apache.karaf.features")
-				.artifactId("standard")
-				.version(karafVersion())
-				.classifier("features")
-				.type("xml");
-		MavenUrlReference gossCoreRepo = maven()
-				.groupId("pnnl.goss")
-				.artifactId("goss-core-feature")
-				.versionAsInProject()
-				.classifier("features")
-				.type("xml");
-		
-		
-		return new Option[] {
-				// KarafDistributionOption.debugConfiguration("5005", true),
-				karafDistributionConfiguration()
-				.frameworkUrl(karafUrl)
-				.unpackDirectory(new File("target/exam"))
-				.useDeployFolder(false),
-				keepRuntimeFolder(),
-				KarafDistributionOption.features(karafStandardRepo, "scr")
-//				,
-//				KarafDistributionOption.features(gossCoreRepo, "goss-core-feature")		
-		};
-	}
-	
-	public static String karafVersion() {
-		ConfigurationManager cm = new ConfigurationManager();
-		String karafVersion = cm.getProperty("pax.exam.karaf.version", "3.0.2");
-		return karafVersion;
-	}
-	
-	@Test
-	public void testRun(){
-		Assert.assertTrue(true);
-	}
+
+//    @Inject
+//    protected GossRequestHandlerRegistrationService requestHandlerService;
+
+    @Configuration
+    public Option[] config() {
+        MavenArtifactUrlReference karafUrl = maven()
+            .groupId("org.apache.karaf")
+            .artifactId("apache-karaf")
+            .version(karafVersion())
+            .type("zip");
+
+        MavenUrlReference karafStandardRepo = maven()
+            .groupId("org.apache.karaf.features")
+            .artifactId("standard")
+            .version(karafVersion())
+            .classifier("features")
+            .type("xml");
+        
+        MavenUrlReference gossCoreRepo = maven()
+                .groupId("pnnl.goss")
+                .artifactId("goss-feature")
+                //.versionAsInProject()
+                .version("0.1.6-SNAPSHOT")
+                .classifier("features")
+                .type("xml");
+        return new Option[] {
+            // KarafDistributionOption.debugConfiguration("5005", true),
+            karafDistributionConfiguration()
+                .frameworkUrl(karafUrl)
+                .unpackDirectory(new File("target/exam"))
+                .useDeployFolder(false),
+            keepRuntimeFolder(),
+            KarafDistributionOption.features(karafStandardRepo , "scr"),
+            mavenBundle("eu.infomas", "annotation-detector","3.0.4"),
+            mavenBundle("org.apache.servicemix.bundles", "org.apache.servicemix.bundles.xstream", "1.4.3_1"),
+            mavenBundle("org.apache.servicemix.bundles", "org.apache.servicemix.bundles.commons-io", "1.4_3"),
+            mavenBundle("org.apache.servicemix.bundles", "org.apache.servicemix.bundles.commons-dbcp", "1.4_3"),
+            mavenBundle("org.apache.servicemix.bundles", "org.apache.servicemix.bundles.reflections", "0.9.8_1"),
+            mavenBundle("com.google.code.gson", "gson", "2.3"),
+            mavenBundle("mysql", "mysql-connector-java", "5.1.33"),
+            mavenBundle("org.apache.httpcomponents", "httpcore-osgi", "4.3.3"),
+            mavenBundle("org.apache.httpcomponents", "httpclient-osgi", "4.3.3"),
+            mavenBundle("org.fusesource.stompjms", "stompjms-client", "1.19"),
+      		mavenBundle("org.fusesource.hawtdispatch", "hawtdispatch-transport", "1.21"),
+      		mavenBundle("org.fusesource.hawtdispatch", "hawtdispatch", "1.21"),
+      		mavenBundle("org.fusesource.hawtbuf", "hawtbuf", "1.11"),
+      		
+//            KarafDistributionOption.features(gossCoreRepo, "goss-core-feature")
+//            mavenBundle()
+//                .groupId("org.ops4j.pax.exam.samples")
+//                .artifactId("pax-exam-sample8-ds")
+//                .versionAsInProject().start(),
+       };
+    }
+
+    public static String karafVersion() {
+        ConfigurationManager cm = new ConfigurationManager();
+        String karafVersion = cm.getProperty("pax.exam.karaf.version", "3.0.2");
+        return karafVersion;
+    }
+
+
+    @Test
+    public void testRegistrationServiceIsNotNull() {
+    	Assert.assertTrue(true);
+    }
 }
