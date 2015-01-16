4458491004de4de8e925c1f7942dabe916a5c204
Craig Allwardt <craig.allwardt@pnnl.gov>
2015-01-15 16:34:59 -0800

    Added broker and tests for running integration tests between client and server.

---
+++ goss-core-itests/src/test/java/pnnl/goss/core/itests/ClientSpecs.groovy
@@ -0,0 +1,5 @@
+package pnnl.goss.core.itests
+
+class ClientSpecs {
+
+}
---
+++ goss-core-itests/src/test/java/pnnl/goss/core/test/utils/AbstractActivemqBrokerTest.java
@@ -0,0 +1,34 @@
+package pnnl.goss.core.test.utils;
+
+
+import org.apache.activemq.broker.BrokerService;
+import org.apache.activemq.broker.Connection;
+import org.apache.activemq.broker.TransportConnector;
+import org.junit.After;
+import org.junit.Before;
+
+public abstract class AbstractActivemqBrokerTest {
+	
+	protected BrokerService broker;
+	protected Connection connection;
+    protected TransportConnector connector;
+//    protected int consumersBeforeDispatchStarts;
+//    protected int timeBeforeDispatchStarts;
+    
+	@Before
+	public void setup() throws Exception{
+		broker = new BrokerService();
+		broker.setPersistent(false);
+		broker.setUseJmx(false);
+		connector = broker.addConnector("tcp://0.0.0.0:59595");
+		broker.start();
+	}
+	
+	@After
+	public void teardown() throws Exception{
+		connection.stop();
+		connector.stop();
+		broker.stop();
+	}
+
+}
