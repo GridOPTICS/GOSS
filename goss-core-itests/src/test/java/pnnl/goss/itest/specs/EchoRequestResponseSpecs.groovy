d01a780d02dec323cad963ebbc502c7c50e0bd90
Craig Allwardt <craig.allwardt@pnnl.gov>
2015-01-20 10:54:12 -0800

    Added xbean so that we can use real versions of activemq for integration tests.

--- goss-core-itests/src/test/java/pnnl/goss/itest/specs/EchoRequestResponseSpecs.groovy
+++ goss-core-itests/src/test/java/pnnl/goss/itest/specs/EchoRequestResponseSpecs.groovy
@@ -1,5 +1,9 @@
 package pnnl.goss.itest.specs
 
+import static pnnl.goss.core.GossCoreContants.PROP_ACTIVEMQ_CONFIG;
+import static pnnl.goss.core.GossCoreContants.PROP_OPENWIRE_URI;
+import static pnnl.goss.core.GossCoreContants.PROP_STOMP_URI;
+
 import pnnl.goss.core.DataResponse
 import pnnl.goss.core.client.internal.GossClient
 import pnnl.goss.core.server.internal.GossDataServicesImpl
@@ -9,46 +13,54 @@ import pnnl.goss.itest.requests.EchoRequest
 import spock.lang.Specification
 
 class EchoRequestResponseSpecs extends Specification {
-	
-	GossClient client
-	GridOpticsServer server;
-	GossDataServicesImpl dataServices
-	GossRequestHandlerRegistrationImpl registrationHandler
-	
-	def "request response specifications"(){
-		expect:
-			assert client != null
-		when: "getting response from hello world EchoRequest"
-			DataResponse response = client.getResponse(new EchoRequest("hello world"))
-		then:
-			assert response != null
-			assert response.data == "hello world"
-			assert response.isResponseComplete() == true
-	}
-	
-	
-	def setup() {
-		dataServices  = new GossDataServicesImpl()
-		registrationHandler = new GossRequestHandlerRegistrationImpl(dataServices)
-		Properties config = new Properties()
-		config.setProperty("goss.openwire.uri", "vm://echotestopenwire")
-		config.setProperty("goss.stomp.uri", "vm://echoteststomp")
-		// config.setProperty(GossCoreContants.PROP_SYSTEM_USER, "fuzzy")
-		// config.setProperty(GossCoreContants.PROP_SYSTEM_PASSWORD, "buckets")
-		registrationHandler.setCoreServerConfig(config)
-		registrationHandler.addHandlersFromClassPath();
-		server = new GridOpticsServer(registrationHandler, true)
-		client = new GossClient(config)
-	}
-	
-	def cleanup() {
-		client.close()
-		client = null
-		registrationHandler.shutdown()
-		registrationHandler = null
-		server.close()
-		dataServices = null
-		
-	}
+
+    static GossClient client
+    static GridOpticsServer server;
+    static GossRequestHandlerRegistrationImpl registrationHandler
+    static GossDataServicesImpl dataServices
+
+    def "request response specifications"(){
+        expect:
+            assert client != null
+        when: "getting response from hello world EchoRequest"
+            DataResponse response = client.getResponse(new EchoRequest("hello world"))
+        then:
+            assert response != null
+            assert response.data == "hello world"
+            assert response.isResponseComplete() == true
+    }
+
+
+
+
+
+
+    def setupSpec() {
+        dataServices  = new GossDataServicesImpl()
+        registrationHandler = new GossRequestHandlerRegistrationImpl(dataServices)
+        Properties config = new Properties()
+        config.setProperty(PROP_OPENWIRE_URI, "tcp://0.0.0.0:51515")
+        config.setProperty(PROP_STOMP_URI, "stomp://0.0.0.0:51516")
+        //def file = new File(getClass().protectionDomain.codeSource.location.path).parent
+//        println file
+        config.setProperty(PROP_ACTIVEMQ_CONFIG, "build/resources/test/test-broker-no-security.xml")
+        // config.setProperty(GossCoreContants.PROP_SYSTEM_USER, "fuzzy")
+        // config.setProperty(GossCoreContants.PROP_SYSTEM_PASSWORD, "buckets")
+        registrationHandler.setCoreServerConfig(config)
+        registrationHandler.addHandlersFromClassPath();
+        server = new GridOpticsServer(registrationHandler, true)
+        client = new GossClient(config)
+
+    }
+
+    def cleanupSpec() {
+        client.close()
+        client = null
+        registrationHandler.shutdown()
+        registrationHandler = null
+        server.close()
+        dataServices = null
+
+    }
 
 }
