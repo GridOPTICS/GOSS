2c8a3068f28ea347525b0ead94a7b5242b12eb9d
Craig Allwardt <craig.allwardt@pnnl.gov>
2015-02-20 13:07:12 -0800

    Registry handler now handles both the registration of uploads and requests.

--- pnnl.goss.core/src/pnnl/goss/core/server/impl/HandlerRegistryImpl.java
+++ pnnl.goss.core/src/pnnl/goss/core/server/impl/HandlerRegistryImpl.java
@@ -1,5 +1,6 @@
 package pnnl.goss.core.server.impl;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -9,6 +10,8 @@ import java.util.concurrent.ConcurrentHashMap;
 import org.apache.felix.dm.annotation.api.Component;
 import org.apache.felix.dm.annotation.api.ServiceDependency;
 import org.osgi.framework.ServiceReference;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import pnnl.goss.core.Request;
 import pnnl.goss.core.RequestAsync;
@@ -17,39 +20,62 @@ import pnnl.goss.core.ResponseError;
 import pnnl.goss.core.UploadRequest;
 import pnnl.goss.core.server.HandlerNotFoundException;
 import pnnl.goss.core.server.RequestHandler;
+import pnnl.goss.core.server.RequestHandlerInterface;
+import pnnl.goss.core.server.RequestUploadHandler;
 import pnnl.goss.core.server.RequestHandlerRegistry;
 
 @Component
 public class HandlerRegistryImpl implements RequestHandlerRegistry {
+	private static final Logger log = LoggerFactory.getLogger(HandlerRegistryImpl.class);
 	
-	private final Map<ServiceReference<RequestHandler>, RequestHandler> registered = new ConcurrentHashMap<>();
+	private final Map<ServiceReference<RequestHandler>, RequestHandler> registeredHandlers = new ConcurrentHashMap<>();
+	private final Map<ServiceReference<RequestUploadHandler>, RequestUploadHandler> registeredUploadHandlers = new ConcurrentHashMap<>();
+	
 	private final Map<String, RequestHandler> handlers = new ConcurrentHashMap<>();
-		
-	@ServiceDependency(removed="handlerRemoved")
+	private final Map<String, RequestUploadHandler> uploadHandlers = new ConcurrentHashMap<>();
+	
+			
+	@ServiceDependency(removed="handlerRemoved", required=false)
 	public void handlerAdded(ServiceReference<RequestHandler> ref, RequestHandler handler){
 		System.out.println("Registering Service");
-		registered.put(ref, handler);
-		handler.getHandles().forEach(p->handlers.put(p, handler));
+		registeredHandlers.put(ref, handler);
+		handler.getHandles().forEach(p->handlers.put(p.getName(), handler));
 	}
 	
 	public void handlerRemoved(ServiceReference<RequestHandler> ref){
 		System.out.println("Un-Registering Service");
-		registered.get(ref).getHandles().forEach(p->handlers.remove(p));
-		registered.remove(ref);
+		registeredHandlers.get(ref).getHandles().forEach(p->handlers.remove(p));
+		registeredHandlers.remove(ref);
 	}
+	
+	
+	@ServiceDependency(removed="uploadHandlerRemoved", required=false)
+	public void uploadHandlerAdded(ServiceReference<RequestUploadHandler> ref, RequestUploadHandler uploadHandler){
+		System.out.println("Registering Upload Service");
+		registeredUploadHandlers.put(ref, uploadHandler);
+		uploadHandler.getHandlerDataTypes().forEach(p->uploadHandlers.put(p, uploadHandler));
+	}
+	
+	public void uploadHandlerRemoved(ServiceReference<RequestUploadHandler> ref){
+		System.out.println("Un-Registering Upload Service");
+		registeredUploadHandlers.get(ref).getHandlerDataTypes().forEach(p->uploadHandlers.remove(p));
+		registeredUploadHandlers.remove(ref);
+	}
+
 
 	@Override
 	public RequestHandler getHandler(Class<? extends Request> request) throws HandlerNotFoundException {
-		
+		log.debug("getHandler for class: "+request.getName());
 		Optional<RequestHandler> maybeHandler = Optional.ofNullable(handlers.get(request.getName()));	
-		return maybeHandler.orElseThrow(HandlerNotFoundException::new);
+		return maybeHandler.orElseThrow(()-> new HandlerNotFoundException(request));
 		
 	}
 
 	@Override
-	public List<RequestHandler> list() {
-		ArrayList<RequestHandler> items = new ArrayList<>();
-		registered.values().forEach(p->items.add(p));
+	public List<RequestHandlerInterface> list() {
+		ArrayList<RequestHandlerInterface> items = new ArrayList<>();
+		registeredHandlers.values().forEach(p->items.add(p));
+		registeredUploadHandlers.values().forEach(p->items.add(p));
 		return items;
 	}
 
@@ -61,9 +87,10 @@ public class HandlerRegistryImpl implements RequestHandlerRegistry {
 	}
 
 	@Override
-	public Response handle(UploadRequest request, String datatype) throws HandlerNotFoundException {
-		RequestHandler handler = getHandler(request.getClass());
-		return handler.handle(request);
+	public Response handle(String dataType, Serializable data) throws HandlerNotFoundException {
+		RequestUploadHandler handler = Optional.ofNullable(uploadHandlers.get(dataType))
+											.orElseThrow(()-> new HandlerNotFoundException(dataType));
+		return handler.upload(dataType, data);
 	}
 
 	@Override
