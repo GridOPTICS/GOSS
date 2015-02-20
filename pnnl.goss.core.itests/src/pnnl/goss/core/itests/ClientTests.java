7cfc585fff53bdbc690491e04b84382e683a5c24
Craig Allwardt <craig.allwardt@pnnl.gov>
2015-02-20 00:24:39 -0800

    Added the itest project to working back into the repository.

---
+++ pnnl.goss.core.itests/src/pnnl/goss/core/itests/ClientTests.java
@@ -0,0 +1,108 @@
+package pnnl.goss.core.itests;
+
+import static org.amdatu.testing.configurator.TestConfigurator.cleanUp;
+import static org.amdatu.testing.configurator.TestConfigurator.configuration;
+import static org.amdatu.testing.configurator.TestConfigurator.configure;
+import static org.amdatu.testing.configurator.TestConfigurator.serviceDependency;
+import static org.junit.Assert.*;
+
+import java.util.concurrent.TimeUnit;
+
+import org.amdatu.testing.configurator.TestConfiguration;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.northconcepts.exception.SystemException;
+
+import pnnl.goss.core.Client;
+import pnnl.goss.core.Client.PROTOCOL;
+import pnnl.goss.core.ClientFactory;
+import pnnl.goss.core.DataResponse;
+import pnnl.goss.core.Response;
+import pnnl.goss.core.server.ServerControl;
+import pnnl.goss.core.server.tester.requests.EchoRequest;
+
+public class ClientTests {
+	
+	private static final Logger log = LoggerFactory.getLogger(ClientTests.class);
+	private TestConfiguration testConfig;
+	private volatile ClientFactory clientFactory;
+	private volatile ServerControl serverControl;
+	
+	
+	private static final String OPENWIRE_CLIENT_CONNECTION = "tcp://localhost:6000";
+	private static final String STOMP_CLIENT_CONNECTION = "tcp://localhost:6000";
+
+	@Before
+	public void before() throws InterruptedException{	
+		testConfig = configure(this)
+						.add(configuration("pnnl.goss.core.server")
+								.set("goss.openwire.uri", "tcp://localhost:6000")
+								.set("goss.stomp.uri",  "tcp://localhost:6001") //vm:(broker:(tcp://localhost:6001)?persistent=false)?marshal=false")
+								.set("goss.start.broker", "true")
+								.set("goss.broker.uri", "tcp://localhost:6000"))
+						.add(serviceDependency(ServerControl.class))
+						.add(configuration(ClientFactory.CONFIG_PID)
+								.set("goss.openwire.uri", "tcp://localhost:6000")
+								.set("goss.stomp.uri",  "tcp://localhost:6001"))
+						.add(serviceDependency(ClientFactory.class));
+		testConfig.apply();
+		
+		// Configuration update is asyncronous, so give a bit of time to catch up
+		TimeUnit.MILLISECONDS.sleep(500);
+	}
+	
+	@Test
+	public void serverCanStartSuccessfully() {
+		assertNotNull(serverControl);
+	}
+		
+	@Test
+	public void clientFactoryRegistryOk(){
+		
+		assertNotNull(clientFactory);	
+		Client client = clientFactory.create(PROTOCOL.OPENWIRE);
+		assertNotNull(client);
+		assertEquals(PROTOCOL.OPENWIRE, client.getProtocol());
+		
+	}
+	
+	@Test
+	public void clientCanGetEcho(){
+		try{
+			String message = "hello world!";
+			Client client = clientFactory.create(PROTOCOL.OPENWIRE);
+			EchoRequest request = new EchoRequest(message);
+			Response response = client.getResponse(request);
+			
+			assertNotNull(response);
+			assertTrue(response instanceof DataResponse);
+			DataResponse dataResponse = (DataResponse)response;
+			assertEquals(message, dataResponse.getData().toString());
+		}
+		catch (SystemException e){
+			e.printStackTrace();
+			throw(e);
+		}
+	}	
+	
+		
+	@After
+	public void after(){
+		try {
+			if (serverControl != null) {serverControl.stop();}
+			cleanUp(this);
+		}
+		catch (Exception e) {
+			System.err.println("Ignoring exception!");
+		}
+		finally {
+			if (clientFactory != null){
+				clientFactory.destroy();
+			}
+		}
+	}
+}
--- pnnl.goss.core.tester/src/pnnl/goss/core/server/tester/EchoCommands.java
+++ pnnl.goss.core.tester/src/pnnl/goss/core/server/tester/EchoCommands.java
@@ -5,28 +5,53 @@ import org.apache.felix.dm.annotation.api.Property;
 import org.apache.felix.dm.annotation.api.ServiceDependency;
 import org.apache.felix.service.command.CommandProcessor;
 
+import pnnl.goss.core.Client;
+import pnnl.goss.core.Client.PROTOCOL;
+import pnnl.goss.core.ClientFactory;
 import pnnl.goss.core.DataResponse;
 import pnnl.goss.core.Response;
+import pnnl.goss.core.server.HandlerNotFoundException;
 import pnnl.goss.core.server.RequestHandlerRegistry;
 import pnnl.goss.core.server.tester.requests.EchoRequest;
 
 @Component(properties={
 		@Property(name=CommandProcessor.COMMAND_SCOPE, value="gt"),
-		@Property(name=CommandProcessor.COMMAND_FUNCTION, value={"echo"})
+		@Property(name=CommandProcessor.COMMAND_FUNCTION, value={"echo", "echoOpenwire"})
 }, provides=Object.class)
 public class EchoCommands {
 
 	@ServiceDependency
 	private volatile RequestHandlerRegistry registry;
 	
+	@ServiceDependency
+	private volatile ClientFactory clientFactory;
+	
+	private EchoRequest buildRequest(String message){
+		return new EchoRequest(message);
+	}
+	
 	public void echo(String message) {
-		EchoRequest request = new EchoRequest(message);
-		Response response = registry.handle(request);
+		Response response = null;
+		try {
+			response = registry.handle(buildRequest(message));
+		} catch (HandlerNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		if (response instanceof DataResponse){
 			System.out.println("Response was: " + ((DataResponse)response).getData());
 		}
 		else{
 			System.out.println("Response wasn't DataResponse it was: "+response.getClass().getName());
 		}
+	}
+	
+	public void echoOpenwire(String message){
+		
+		Client client = clientFactory.create(PROTOCOL.OPENWIRE);
+		
+		Response response = client.getResponse(buildRequest(message));
+		
+		
 	}
 }
