e12a706ce8ec718ecce665945116d46dec92744d
Craig Allwardt <craig.allwardt@pnnl.gov>
2015-03-06 15:48:59 -0800

    Added DataSource testing to the integration tests.

--- pnnl.goss.core.itests/src/pnnl/goss/core/itests/DataSourceTesting.java
+++ pnnl.goss.core.itests/src/pnnl/goss/core/itests/DataSourceTesting.java
@@ -7,6 +7,9 @@ import static org.amdatu.testing.configurator.TestConfigurator.serviceDependency
 import static pnnl.goss.core.itests.TestSteps.configureServerAndClientPropertiesConfig;
 import static org.junit.Assert.*;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
@@ -23,6 +26,7 @@ import org.junit.Test;
 import pnnl.goss.core.ClientFactory;
 //import pnnl.goss.core.security.PermissionAdapter;
 import pnnl.goss.core.server.DataSourceObject;
+import pnnl.goss.core.server.DataSourcePooledJdbc;
 import pnnl.goss.core.server.DataSourceRegistry;
 import pnnl.goss.core.server.DataSourceType;
 import pnnl.goss.core.server.ServerControl;
@@ -58,12 +62,31 @@ public class DataSourceTesting {
 		assertNotNull(registry);
 		Map<String, DataSourceType> available = registry.getAvailable();
 		assertNotNull(available);
-		assertTrue(available.size() == 1);
+		assertTrue(available.size() > 0);
 		assertNotNull(available.get("pnnl.goss.core.server.runner.datasource.CommandLogDataSource"));
 		DataSourceObject obj = registry.get("pnnl.goss.core.server.runner.datasource.CommandLogDataSource");
-		assertEquals(DataSourceType.DS_TYPE_OTHER, obj.getDataSourceType());
-		//CommandLogDataSource ds = available.get("pnnl.goss.core.server.runner.datasource.CommandLogDataSource");
-		
+		assertEquals(DataSourceType.DS_TYPE_OTHER, obj.getDataSourceType());				
+	}
+	
+	@Test
+	public void canCreateTableOnConnection(){
+		DataSourceObject obj = registry.get("pnnl.goss.core.server.runner.datasource.H2TestDataSource");
+		assertNotNull(obj);
+		assertEquals(DataSourceType.DS_TYPE_JDBC, obj.getDataSourceType());
+		assertTrue(obj instanceof DataSourcePooledJdbc);
+		DataSourcePooledJdbc ds = (DataSourcePooledJdbc)obj;
+		try (Connection conn = ds.getConnection()) {
+			try (Statement stmt = conn.createStatement()){
+				stmt.execute(
+					"CREATE TABLE actual_wind_total "
+						+ "(TimeStamp datetime NOT NULL, Wind decimal(28,10) DEFAULT NULL, PRIMARY KEY (TimeStamp));");
+				stmt.execute("INSERT INTO actual_wind_total VALUES('2009-01-20 05:05:05', 20203.4232);");
+
+			}
+		} catch (SQLException e1) {
+			e1.printStackTrace();
+			fail();
+		}
 	}
 	
 	
