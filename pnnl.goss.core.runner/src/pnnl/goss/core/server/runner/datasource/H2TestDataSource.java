9d95b329a0d36b08de1559148f738fc4c6288037
Craig A <callwardt@gmail.com>
2015-03-09 22:30:36 -0700

    Updated test datasources for dealing with the new interfaces.

--- pnnl.goss.core.runner/src/pnnl/goss/core/server/runner/datasource/H2TestDataSource.java
+++ pnnl.goss.core.runner/src/pnnl/goss/core/server/runner/datasource/H2TestDataSource.java
@@ -4,29 +4,30 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.Properties;
 
-import javax.sql.CommonDataSource;
 import javax.sql.ConnectionPoolDataSource;
 
 import org.apache.felix.dm.annotation.api.Component;
 import org.apache.felix.dm.annotation.api.ServiceDependency;
 import org.apache.felix.dm.annotation.api.Start;
+import org.apache.felix.dm.annotation.api.Stop;
 import org.h2.util.OsgiDataSourceFactory;
 import org.osgi.service.jdbc.DataSourceFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import pnnl.goss.core.server.AbstractDataSourceObject;
-import pnnl.goss.core.server.AbstractSqlPooledDatasource;
 import pnnl.goss.core.server.DataSourceObject;
 import pnnl.goss.core.server.DataSourcePooledJdbc;
 import pnnl.goss.core.server.DataSourceType;
 
 @Component
-public class H2TestDataSource extends AbstractSqlPooledDatasource implements DataSourceObject, DataSourcePooledJdbc {
+public class H2TestDataSource implements DataSourcePooledJdbc, DataSourceObject {
 	private static final Logger log = LoggerFactory.getLogger(H2TestDataSource.class);
 	
+	// Use an osgi connection factory.
 	@ServiceDependency(name="org.h2.util.OsgiDataSourceFactory")
-	private  volatile DataSourceFactory factory;
+	private volatile DataSourceFactory factory;
+	
+	private ConnectionPoolDataSource pooledDataSource;
 	
 	@Start
 	public void start() {
@@ -37,19 +38,34 @@ public class H2TestDataSource extends AbstractSqlPooledDatasource implements Dat
 		properties.setProperty(OsgiDataSourceFactory.JDBC_PASSWORD, "sa");
 		
 		try {
-			setDataSource(factory.createConnectionPoolDataSource(properties));
-			log.debug("getConnection().isClosed(): " + getConnection().isClosed());
+			pooledDataSource = factory.createConnectionPoolDataSource(properties);
+			log.debug("Connection pool datasource created for: " + properties.getProperty("url"));
+			
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		//System.out.println("factory is? "+factory);
 	}
+	
+	@Stop
+	public void stop(){
+		pooledDataSource = null;
+	}
 
 	@Override
-	public void onRemoved() {
-		// TODO Auto-generated method stub
-		
+	public String getName() {
+		return this.getClass().getName();
+	}
+
+	@Override
+	public DataSourceType getDataSourceType() {
+		return DataSourceType.DS_TYPE_JDBC;
+	}
+
+	@Override
+	public Connection getConnection() throws SQLException {
+		return pooledDataSource.getPooledConnection().getConnection();
 	}
 	
 }
