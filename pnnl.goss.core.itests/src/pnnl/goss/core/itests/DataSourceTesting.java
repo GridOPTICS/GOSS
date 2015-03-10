148d5af9fa0d6fb25239a562c7f9a95019a6b78a
Craig A <callwardt@gmail.com>
2015-03-09 23:02:36 -0700

    Added required commons pool to required classes.

--- pnnl.goss.core.itests/src/pnnl/goss/core/itests/DataSourceTesting.java
+++ pnnl.goss.core.itests/src/pnnl/goss/core/itests/DataSourceTesting.java
@@ -4,8 +4,10 @@ import static org.amdatu.testing.configurator.TestConfigurator.cleanUp;
 import static org.amdatu.testing.configurator.TestConfigurator.configuration;
 import static org.amdatu.testing.configurator.TestConfigurator.configure;
 import static org.amdatu.testing.configurator.TestConfigurator.serviceDependency;
-import static pnnl.goss.core.itests.TestSteps.configureServerAndClientPropertiesConfig;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -14,31 +16,27 @@ import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 import org.amdatu.testing.configurator.TestConfiguration;
-import org.apache.shiro.mgt.SecurityManager;
 import org.junit.After;
 import org.junit.Before;
-
-
-
-
 import org.junit.Test;
 
-import pnnl.goss.core.ClientFactory;
+import pnnl.goss.core.server.DataSourceBuilder;
 //import pnnl.goss.core.security.PermissionAdapter;
 import pnnl.goss.core.server.DataSourceObject;
 import pnnl.goss.core.server.DataSourcePooledJdbc;
 import pnnl.goss.core.server.DataSourceRegistry;
 import pnnl.goss.core.server.DataSourceType;
-import pnnl.goss.core.server.ServerControl;
 
 public class DataSourceTesting {
 
 	public volatile DataSourceRegistry registry;
+	public volatile DataSourceBuilder builder;
+	
 	private TestConfiguration testConfig;
 	
 	@Before
 	public void before() throws InterruptedException{
-		TestConfiguration testConfig = configure(this)
+		testConfig = configure(this)
 				.add(TestSteps.configureServerAndClientPropertiesConfig())
 				
 				.add(configuration("pnnl.goss.core.security.propertyfile")
@@ -49,6 +47,7 @@ public class DataSourceTesting {
 				//.add(serviceDependency(ServerControl.class))
 				//.add(serviceDependency(ClientFactory.class))				
 //				.add(TestSteps.configureServerAndClientPropertiesConfig())
+				.add(serviceDependency(DataSourceBuilder.class))
 				.add(serviceDependency(DataSourceRegistry.class));
 				//.add(serviceDependency(SecurityManager.class));
 		testConfig.apply();
@@ -68,6 +67,41 @@ public class DataSourceTesting {
 		DataSourceObject obj = registry.get("pnnl.goss.core.server.runner.datasource.CommandLogDataSource");
 		assertEquals(DataSourceType.DS_TYPE_OTHER, obj.getDataSourceType());
 		System.out.println("TEST_END: canGetLogDataSource");
+	}
+	
+	@Test
+	public void canCreateTableOnBasicDataSourceConnection(){
+		System.out.println("TEST: canCreateTableOnBasicDataSourceConnection");
+		assertNotNull("Builder was null", builder);
+		String dbName = "A Special Database"; // key for looking up the datasourceobject.
+		try {
+			builder.create(dbName, "jdbc:h2:mem:fusion3", "sa", "sa", "org.h2.Driver");
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail("An exception occurred creating the datasource.");
+		}
+		
+		assertNotNull("Datasource registry null", registry);
+		
+		DataSourcePooledJdbc obj = (DataSourcePooledJdbc) registry.get(dbName);
+		assertNotNull("DataSourcePooledJdbc was null after registry.get", obj);
+		
+		assertEquals(DataSourceType.DS_TYPE_JDBC, obj.getDataSourceType());
+		assertTrue(obj instanceof DataSourcePooledJdbc);
+		DataSourcePooledJdbc ds = (DataSourcePooledJdbc)obj;
+		try (Connection conn = ds.getConnection()) {
+			try (Statement stmt = conn.createStatement()){
+				stmt.execute(
+					"CREATE TABLE actual_wind_total "
+						+ "(TimeStamp datetime NOT NULL, Wind decimal(28,10) DEFAULT NULL, PRIMARY KEY (TimeStamp));");
+				stmt.execute("INSERT INTO actual_wind_total VALUES('2009-01-20 05:05:05', 20203.4232);");
+
+			}
+		} catch (SQLException e1) {
+			e1.printStackTrace();
+			fail();
+		}
+		System.out.println("TEST_END: canCreateTableOnBasicDataSourceConnection");
 	}
 	
 	@Test
