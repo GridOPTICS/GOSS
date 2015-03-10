79fd478e2a99436cf622c9250ee8e38e2de847af
Craig A <callwardt@gmail.com>
2015-03-09 22:30:08 -0700

    Modified DataSource object interfaces.

--- pnnl.goss.core/src/pnnl/goss/core/server/DataSourcePooledJdbc.java
+++ pnnl.goss.core/src/pnnl/goss/core/server/DataSourcePooledJdbc.java
@@ -3,12 +3,8 @@ package pnnl.goss.core.server;
 import java.sql.Connection;
 import java.sql.SQLException;
 
-import javax.sql.ConnectionPoolDataSource;
-
 public interface DataSourcePooledJdbc extends DataSourceObject {
 
 	Connection getConnection() throws SQLException;
-	
-	void setDataSource(ConnectionPoolDataSource datasource);
 
 }
--- pnnl.goss.core/src/pnnl/goss/core/server/DataSourceRegistry.java
+++ pnnl.goss.core/src/pnnl/goss/core/server/DataSourceRegistry.java
@@ -5,13 +5,31 @@ import java.util.Map;
 public interface DataSourceRegistry {
 	
 	/**
-	 * Get a Datasourceobject from the registry.  If a key
+	 * Get a DataSourceObject from the registry.  If a key
 	 * does not exist then this call should return null.
 	 * 
+	 * @param  
 	 * @param key
 	 * @return
 	 */
 	public DataSourceObject get(String key);
+	
+	/**
+	 * Adds a DataSourceObject to the registry, making it available for
+	 * the entire system.
+	 * 
+	 * @param key
+	 * @param obj
+	 */
+	public void add(String key, DataSourceObject obj);
+	
+	/**
+	 * Remove DataSourceObject from the registry.  If the object doesn't 
+	 * exist this function is silent.
+	 * 
+	 * @param key
+	 */
+	public void remove(String key);
 	
 	/**
 	 * Retrieve a map of names-> datasourcetype that can be retrieved
