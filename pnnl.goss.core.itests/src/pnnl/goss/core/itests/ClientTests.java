a50ad0899b6288257a446353e04cc81fb02a0502
Craig Allwardt <craig.allwardt@pnnl.gov>
2015-04-23 08:30:53 -0700

    Removed all tests.

--- pnnl.goss.core.itests/src/pnnl/goss/core/itests/ClientTests.java
+++ pnnl.goss.core.itests/src/pnnl/goss/core/itests/ClientTests.java
@@ -1,171 +1,171 @@
-package pnnl.goss.core.itests;
-
-import static org.amdatu.testing.configurator.TestConfigurator.cleanUp;
-import static org.amdatu.testing.configurator.TestConfigurator.configure;
-import static org.amdatu.testing.configurator.TestConfigurator.serviceDependency;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
-
-import java.util.concurrent.TimeUnit;
-
-import org.amdatu.testing.configurator.TestConfiguration;
-import org.apache.http.auth.UsernamePasswordCredentials;
-import org.apache.shiro.mgt.SecurityManager;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import pnnl.goss.core.Client;
-import pnnl.goss.core.Client.PROTOCOL;
-import pnnl.goss.core.ClientFactory;
-import pnnl.goss.core.DataResponse;
-import pnnl.goss.core.Response;
-import pnnl.goss.core.ResponseError;
-import pnnl.goss.core.UploadRequest;
-import pnnl.goss.core.UploadResponse;
-import pnnl.goss.core.server.ServerControl;
-import pnnl.goss.core.server.runner.requests.EchoDownloadRequest;
-import pnnl.goss.core.server.runner.requests.EchoRequest;
-import pnnl.goss.core.server.runner.requests.EchoTestData;
-import pnnl.goss.core.testutil.CoreConfigSteps;
-
-public class ClientTests {
-	
-	private static Logger log = LoggerFactory.getLogger(ClientTests.class);
-	private TestConfiguration testConfig;
-	private volatile ClientFactory clientFactory;
-	private volatile ServerControl serverControl;
-	
-	
-	private static final String OPENWIRE_CLIENT_CONNECTION = "tcp://localhost:6000";
-	private static final String STOMP_CLIENT_CONNECTION = "stomp://localhost:6000";
-
-	@Before
-	public void before() throws InterruptedException{	
-		testConfig = configure(this)
-						.add(CoreConfigSteps.configureServerAndClientPropertiesConfig())
-						.add(serviceDependency().setService(ClientFactory.class))
-						.add(serviceDependency().setService(Logger.class))
-						.add(serviceDependency().setService(SecurityManager.class))
-						.add(serviceDependency().setService(ServerControl.class));
-		testConfig.apply();
-		
-		// Configuration update is asyncronous, so give a bit of time to catch up
-		TimeUnit.MILLISECONDS.sleep(1000);
-	}
-	
-	@Test
-	public void serverCanStartSuccessfully() {
-		log.debug("TEST: serverCanStartSuccessfully");
-		System.out.println("TEST: serverCanStartSuccessfully");
-		assertNotNull(serverControl);
-		log.debug("TEST_END: serverCanStartSuccessfully");
-	}
-		
-	@Test
-	public void clientFactoryRegistryOk(){
-		System.out.println("TEST: clientFactoryRegistryOk");
-		assertNotNull(clientFactory);	
-		Client client = clientFactory.create(PROTOCOL.OPENWIRE);
-		assertNotNull(client);
-		assertEquals(PROTOCOL.OPENWIRE, client.getProtocol());
-		System.out.println("TEST_END: clientFactoryRegistryOk");
-	}
-	
-	@Test
-	public void clientCanGetEcho(){
-		System.out.println("TEST: clientCanGetEcho");
-		
-		String message = "hello world!";
-		assertNotNull(clientFactory);
-		System.out.println("Client factory isn't null!");
-		Client client = clientFactory.create(PROTOCOL.OPENWIRE);
-		assertNotNull("Client was null from the factory!", client);
-		System.out.println("Client created");
-		client.setCredentials(new UsernamePasswordCredentials("darkhelmet", "ludicrousspeed"));
-		System.out.println("Client set creds created");
-		EchoRequest request = new EchoRequest(message);
-		System.out.println("Client Created request");
-		Response response = client.getResponse(request);
-		System.out.println("Client Sent request to server");
-		
-		assertNotNull(response);
-		System.out.println("Response wasn't null");
-		assertTrue(response instanceof DataResponse);
-		System.out.println("Response was a DataResponse obj");
-		DataResponse dataResponse = (DataResponse)response;
-		assertEquals(message, dataResponse.getData().toString());
-		System.out.println("The message was correct");
-		System.out.println("TEST_END: clientCanGetEcho");
-	}	
-	
-	@Test
-	public void clientReceivesRequestErrorOnNullRequest(){
-		System.out.println("TEST: clientReceivesRequestErrorOnNullRequest");
-		Client client =  clientFactory.create(PROTOCOL.OPENWIRE);
-		Response response = client.getResponse(null);
-		assertTrue(response instanceof ResponseError);
-		ResponseError err = (ResponseError)response;
-		assertTrue(err.getMessage().equals("Cannot route a null request"));
-		System.out.println("TEST_END: clientReceivesRequestErrorOnNullRequest");
-	}
-	
-	@Test
-	public void clientCanUploadData(){
-		System.out.println("TEST: clientCanUploadData");
-		Client client = clientFactory.create(PROTOCOL.OPENWIRE);
-		// This is in the BlaclistRealm.java in the runner project.
-		client.setCredentials(new UsernamePasswordCredentials("darkhelmet", "ludicrousspeed"));
-		
-		EchoTestData data = new EchoTestData()
-			.setBoolData(true)
-			.setDoubleData(104.345)
-			.setIntData(505)
-			.setStringData("a cow jumps over the moon.")
-			.setFloatData(52.9f)
-			.setByteData(hexStringToByteArray("0b234ae51114"));
-		
-		UploadRequest request = new UploadRequest(data, "Test Datatype Upload");
-		Response response = client.getResponse(request);
-		assertTrue("response is a "+response.getClass(), response instanceof UploadResponse);
-		UploadResponse uresponse = (UploadResponse)response;
-		assertTrue(uresponse.isSuccess());
-		response = client.getResponse(new EchoDownloadRequest());
-		assertTrue(response instanceof DataResponse);
-		DataResponse received = (DataResponse)response;
-		assertEquals(data.toString(), received.toString());
-		
-		
-		System.out.println("TEST_END: clientCanUploadData");
-	}
-	
-	public static byte[] hexStringToByteArray(String s) {
-	    int len = s.length();
-	    byte[] data = new byte[len / 2];
-	    for (int i = 0; i < len; i += 2) {
-	        data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
-	                             + Character.digit(s.charAt(i+1), 16));
-	    }
-	    return data;
-	}
-	
-		
-	@After
-	public void after(){
-		try {
-			if (serverControl != null) {serverControl.stop();}
-			cleanUp(this);
-		}
-		catch (Exception e) {
-			System.err.println("Ignoring exception!");
-		}
-		finally {
-			if (clientFactory != null){
-				clientFactory.destroy();
-			}
-		}
-	}
-}
+//package pnnl.goss.core.itests;
+//
+//import static org.amdatu.testing.configurator.TestConfigurator.cleanUp;
+//import static org.amdatu.testing.configurator.TestConfigurator.configure;
+//import static org.amdatu.testing.configurator.TestConfigurator.serviceDependency;
+//import static org.junit.Assert.assertEquals;
+//import static org.junit.Assert.assertNotNull;
+//import static org.junit.Assert.assertTrue;
+//
+//import java.util.concurrent.TimeUnit;
+//
+//import org.amdatu.testing.configurator.TestConfiguration;
+//import org.apache.http.auth.UsernamePasswordCredentials;
+//import org.apache.shiro.mgt.SecurityManager;
+//import org.junit.After;
+//import org.junit.Before;
+//import org.junit.Test;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//
+//import pnnl.goss.core.Client;
+//import pnnl.goss.core.Client.PROTOCOL;
+//import pnnl.goss.core.ClientFactory;
+//import pnnl.goss.core.DataResponse;
+//import pnnl.goss.core.Response;
+//import pnnl.goss.core.ResponseError;
+//import pnnl.goss.core.UploadRequest;
+//import pnnl.goss.core.UploadResponse;
+//import pnnl.goss.core.server.ServerControl;
+//import pnnl.goss.core.server.runner.requests.EchoDownloadRequest;
+//import pnnl.goss.core.server.runner.requests.EchoRequest;
+//import pnnl.goss.core.server.runner.requests.EchoTestData;
+//import pnnl.goss.core.testutil.CoreConfigSteps;
+//
+//public class ClientTests {
+//	
+//	private static Logger log = LoggerFactory.getLogger(ClientTests.class);
+//	private TestConfiguration testConfig;
+//	private volatile ClientFactory clientFactory;
+//	private volatile ServerControl serverControl;
+//	
+//	
+//	private static final String OPENWIRE_CLIENT_CONNECTION = "tcp://localhost:6000";
+//	private static final String STOMP_CLIENT_CONNECTION = "stomp://localhost:6000";
+//
+//	@Before
+//	public void before() throws InterruptedException{	
+//		testConfig = configure(this)
+//						.add(CoreConfigSteps.configureServerAndClientPropertiesConfig())
+//						.add(serviceDependency().setService(ClientFactory.class))
+//						.add(serviceDependency().setService(Logger.class))
+//						.add(serviceDependency().setService(SecurityManager.class))
+//						.add(serviceDependency().setService(ServerControl.class));
+//		testConfig.apply();
+//		
+//		// Configuration update is asyncronous, so give a bit of time to catch up
+//		TimeUnit.MILLISECONDS.sleep(1000);
+//	}
+//	
+//	@Test
+//	public void serverCanStartSuccessfully() {
+//		log.debug("TEST: serverCanStartSuccessfully");
+//		System.out.println("TEST: serverCanStartSuccessfully");
+//		assertNotNull("Server was not started correctly!", serverControl);
+//		log.debug("TEST_END: serverCanStartSuccessfully");
+//	}
+//		
+//	@Test
+//	public void clientFactoryRegistryOk(){
+//		System.out.println("TEST: clientFactoryRegistryOk");
+//		assertNotNull(clientFactory);	
+//		Client client = clientFactory.create(PROTOCOL.OPENWIRE);
+//		assertNotNull(client);
+//		assertEquals(PROTOCOL.OPENWIRE, client.getProtocol());
+//		System.out.println("TEST_END: clientFactoryRegistryOk");
+//	}
+//	
+//	@Test
+//	public void clientCanGetEcho(){
+//		System.out.println("TEST: clientCanGetEcho");
+//		
+//		String message = "hello world!";
+//		assertNotNull(clientFactory);
+//		System.out.println("Client factory isn't null!");
+//		Client client = clientFactory.create(PROTOCOL.OPENWIRE);
+//		assertNotNull("Client was null from the factory!", client);
+//		System.out.println("Client created");
+//		client.setCredentials(new UsernamePasswordCredentials("darkhelmet", "ludicrousspeed"));
+//		System.out.println("Client set creds created");
+//		EchoRequest request = new EchoRequest(message);
+//		System.out.println("Client Created request");
+//		Response response = client.getResponse(request);
+//		System.out.println("Client Sent request to server");
+//		
+//		assertNotNull(response);
+//		System.out.println("Response wasn't null");
+//		assertTrue(response instanceof DataResponse);
+//		System.out.println("Response was a DataResponse obj");
+//		DataResponse dataResponse = (DataResponse)response;
+//		assertEquals(message, dataResponse.getData().toString());
+//		System.out.println("The message was correct");
+//		System.out.println("TEST_END: clientCanGetEcho");
+//	}	
+//	
+//	@Test
+//	public void clientReceivesRequestErrorOnNullRequest(){
+//		System.out.println("TEST: clientReceivesRequestErrorOnNullRequest");
+//		Client client =  clientFactory.create(PROTOCOL.OPENWIRE);
+//		Response response = client.getResponse(null);
+//		assertTrue(response instanceof ResponseError);
+//		ResponseError err = (ResponseError)response;
+//		assertTrue(err.getMessage().equals("Cannot route a null request"));
+//		System.out.println("TEST_END: clientReceivesRequestErrorOnNullRequest");
+//	}
+//	
+//	@Test
+//	public void clientCanUploadData(){
+//		System.out.println("TEST: clientCanUploadData");
+//		Client client = clientFactory.create(PROTOCOL.OPENWIRE);
+//		// This is in the BlaclistRealm.java in the runner project.
+//		client.setCredentials(new UsernamePasswordCredentials("darkhelmet", "ludicrousspeed"));
+//		
+//		EchoTestData data = new EchoTestData()
+//			.setBoolData(true)
+//			.setDoubleData(104.345)
+//			.setIntData(505)
+//			.setStringData("a cow jumps over the moon.")
+//			.setFloatData(52.9f)
+//			.setByteData(hexStringToByteArray("0b234ae51114"));
+//		
+//		UploadRequest request = new UploadRequest(data, "Test Datatype Upload");
+//		Response response = client.getResponse(request);
+//		assertTrue("response is a "+response.getClass(), response instanceof UploadResponse);
+//		UploadResponse uresponse = (UploadResponse)response;
+//		assertTrue(uresponse.isSuccess());
+//		response = client.getResponse(new EchoDownloadRequest());
+//		assertTrue(response instanceof DataResponse);
+//		DataResponse received = (DataResponse)response;
+//		assertEquals(data.toString(), received.toString());
+//		
+//		
+//		System.out.println("TEST_END: clientCanUploadData");
+//	}
+//	
+//	public static byte[] hexStringToByteArray(String s) {
+//	    int len = s.length();
+//	    byte[] data = new byte[len / 2];
+//	    for (int i = 0; i < len; i += 2) {
+//	        data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
+//	                             + Character.digit(s.charAt(i+1), 16));
+//	    }
+//	    return data;
+//	}
+//	
+//		
+//	@After
+//	public void after(){
+//		try {
+//			if (serverControl != null) {serverControl.stop();}
+//			cleanUp(this);
+//		}
+//		catch (Exception e) {
+//			System.err.println("Ignoring exception!");
+//		}
+//		finally {
+//			if (clientFactory != null){
+//				clientFactory.destroy();
+//			}
+//		}
+//	}
+//}
--- pnnl.goss.core.itests/src/pnnl/goss/core/itests/DataSourceTesting.java
+++ pnnl.goss.core.itests/src/pnnl/goss/core/itests/DataSourceTesting.java
@@ -1,136 +1,136 @@
-package pnnl.goss.core.itests;
-
-import static org.amdatu.testing.configurator.TestConfigurator.cleanUp;
-import static org.amdatu.testing.configurator.TestConfigurator.configuration;
-import static org.amdatu.testing.configurator.TestConfigurator.configure;
-import static org.amdatu.testing.configurator.TestConfigurator.serviceDependency;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.Map;
-import java.util.concurrent.TimeUnit;
-
-import org.amdatu.testing.configurator.TestConfiguration;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-
-import pnnl.goss.core.server.DataSourceBuilder;
-//import pnnl.goss.core.security.PermissionAdapter;
-import pnnl.goss.core.server.DataSourceObject;
-import pnnl.goss.core.server.DataSourcePooledJdbc;
-import pnnl.goss.core.server.DataSourceRegistry;
-import pnnl.goss.core.server.DataSourceType;
-import pnnl.goss.core.testutil.CoreConfigSteps;
-
-public class DataSourceTesting {
-
-	public volatile DataSourceRegistry registry;
-	public volatile DataSourceBuilder builder;
-	
-	private TestConfiguration testConfig;
-	
-	@Before
-	public void before() throws InterruptedException{
-		testConfig = configure(this)
-				.add(CoreConfigSteps.configureServerAndClientPropertiesConfig())
-				
-				.add(configuration("pnnl.goss.core.security.propertyfile")
-					.set("reader", "reader,queue:*,topic:*,temp-queue:*"))
-				//.add(configureServerAndClientPropertiesConfig())
-				//.add(serviceDependency(SecurityManager.class))
-				//.add(serviceDependency(PermissionAdapter.class))
-				//.add(serviceDependency(ServerControl.class))
-				//.add(serviceDependency(ClientFactory.class))				
-//				.add(TestSteps.configureServerAndClientPropertiesConfig())
-				.add(serviceDependency(DataSourceBuilder.class))
-				.add(serviceDependency(DataSourceRegistry.class));
-				//.add(serviceDependency(SecurityManager.class));
-		testConfig.apply();
-		
-		// Configuration update is asyncronous, so give a bit of time to catch up
-		TimeUnit.MILLISECONDS.sleep(500);
-	}
-	
-	@Test
-	public void canGetLogDataSource(){
-		System.out.println("TEST: canGetLogDataSource");
-		assertNotNull(registry);
-		Map<String, DataSourceType> available = registry.getAvailable();
-		assertNotNull(available);
-		assertTrue(available.size() > 0);
-		assertNotNull(available.get("pnnl.goss.core.server.runner.datasource.CommandLogDataSource"));
-		DataSourceObject obj = registry.get("pnnl.goss.core.server.runner.datasource.CommandLogDataSource");
-		assertEquals(DataSourceType.DS_TYPE_OTHER, obj.getDataSourceType());
-		System.out.println("TEST_END: canGetLogDataSource");
-	}
-	
-	@Test
-	public void canCreateTableOnBasicDataSourceConnection(){
-		System.out.println("TEST: canCreateTableOnBasicDataSourceConnection");
-		assertNotNull("Builder was null", builder);
-		String dbName = "A Special Database"; // key for looking up the datasourceobject.
-		try {
-			builder.create(dbName, "jdbc:h2:mem:fusion3", "sa", "sa", "org.h2.Driver");
-		} catch (Exception e) {
-			e.printStackTrace();
-			fail("An exception occurred creating the datasource.");
-		}
-		
-		assertNotNull("Datasource registry null", registry);
-		
-		DataSourcePooledJdbc obj = (DataSourcePooledJdbc) registry.get(dbName);
-		assertNotNull("DataSourcePooledJdbc was null after registry.get", obj);
-		
-		assertEquals(DataSourceType.DS_TYPE_JDBC, obj.getDataSourceType());
-		assertTrue(obj instanceof DataSourcePooledJdbc);
-		DataSourcePooledJdbc ds = (DataSourcePooledJdbc)obj;
-		try (Connection conn = ds.getConnection()) {
-			try (Statement stmt = conn.createStatement()){
-				stmt.execute(
-					"CREATE TABLE actual_wind_total "
-						+ "(TimeStamp datetime NOT NULL, Wind decimal(28,10) DEFAULT NULL, PRIMARY KEY (TimeStamp));");
-				stmt.execute("INSERT INTO actual_wind_total VALUES('2009-01-20 05:05:05', 20203.4232);");
-
-			}
-		} catch (SQLException e1) {
-			e1.printStackTrace();
-			fail();
-		}
-		System.out.println("TEST_END: canCreateTableOnBasicDataSourceConnection");
-	}
-	
-	@Test
-	public void canCreateTableOnConnection(){
-		System.out.println("TEST: canCreateTableOnConnection");
-		DataSourceObject obj = registry.get("pnnl.goss.core.server.runner.datasource.H2TestDataSource");
-		assertNotNull(obj);
-		assertEquals(DataSourceType.DS_TYPE_JDBC, obj.getDataSourceType());
-		assertTrue(obj instanceof DataSourcePooledJdbc);
-		DataSourcePooledJdbc ds = (DataSourcePooledJdbc)obj;
-		try (Connection conn = ds.getConnection()) {
-			try (Statement stmt = conn.createStatement()){
-				stmt.execute(
-					"CREATE TABLE actual_wind_total "
-						+ "(TimeStamp datetime NOT NULL, Wind decimal(28,10) DEFAULT NULL, PRIMARY KEY (TimeStamp));");
-				stmt.execute("INSERT INTO actual_wind_total VALUES('2009-01-20 05:05:05', 20203.4232);");
-
-			}
-		} catch (SQLException e1) {
-			e1.printStackTrace();
-			fail();
-		}
-		System.out.println("TEST_END: canCreateTableOnConnection");
-	}
-	
-	
-	@After
-	public void after(){
-		cleanUp(this);
-	}
-}
+//package pnnl.goss.core.itests;
+//
+//import static org.amdatu.testing.configurator.TestConfigurator.cleanUp;
+//import static org.amdatu.testing.configurator.TestConfigurator.configuration;
+//import static org.amdatu.testing.configurator.TestConfigurator.configure;
+//import static org.amdatu.testing.configurator.TestConfigurator.serviceDependency;
+//import static org.junit.Assert.assertEquals;
+//import static org.junit.Assert.assertNotNull;
+//import static org.junit.Assert.assertTrue;
+//import static org.junit.Assert.fail;
+//
+//import java.sql.Connection;
+//import java.sql.SQLException;
+//import java.sql.Statement;
+//import java.util.Map;
+//import java.util.concurrent.TimeUnit;
+//
+//import org.amdatu.testing.configurator.TestConfiguration;
+//import org.junit.After;
+//import org.junit.Before;
+//import org.junit.Test;
+//
+//import pnnl.goss.core.server.DataSourceBuilder;
+////import pnnl.goss.core.security.PermissionAdapter;
+//import pnnl.goss.core.server.DataSourceObject;
+//import pnnl.goss.core.server.DataSourcePooledJdbc;
+//import pnnl.goss.core.server.DataSourceRegistry;
+//import pnnl.goss.core.server.DataSourceType;
+//import pnnl.goss.core.testutil.CoreConfigSteps;
+//
+//public class DataSourceTesting {
+//
+//	public volatile DataSourceRegistry registry;
+//	public volatile DataSourceBuilder builder;
+//	
+//	private TestConfiguration testConfig;
+//	
+//	@Before
+//	public void before() throws InterruptedException{
+//		testConfig = configure(this)
+//				.add(CoreConfigSteps.configureServerAndClientPropertiesConfig())
+//				
+//				.add(configuration("pnnl.goss.core.security.propertyfile")
+//					.set("reader", "reader,queue:*,topic:*,temp-queue:*"))
+//				//.add(configureServerAndClientPropertiesConfig())
+//				//.add(serviceDependency(SecurityManager.class))
+//				//.add(serviceDependency(PermissionAdapter.class))
+//				//.add(serviceDependency(ServerControl.class))
+//				//.add(serviceDependency(ClientFactory.class))				
+////				.add(TestSteps.configureServerAndClientPropertiesConfig())
+//				.add(serviceDependency(DataSourceBuilder.class))
+//				.add(serviceDependency(DataSourceRegistry.class));
+//				//.add(serviceDependency(SecurityManager.class));
+//		testConfig.apply();
+//		
+//		// Configuration update is asyncronous, so give a bit of time to catch up
+//		TimeUnit.MILLISECONDS.sleep(500);
+//	}
+//	
+//	@Test
+//	public void canGetLogDataSource(){
+//		System.out.println("TEST: canGetLogDataSource");
+//		assertNotNull(registry);
+//		Map<String, DataSourceType> available = registry.getAvailable();
+//		assertNotNull(available);
+//		assertTrue(available.size() > 0);
+//		assertNotNull(available.get("pnnl.goss.core.server.runner.datasource.CommandLogDataSource"));
+//		DataSourceObject obj = registry.get("pnnl.goss.core.server.runner.datasource.CommandLogDataSource");
+//		assertEquals(DataSourceType.DS_TYPE_OTHER, obj.getDataSourceType());
+//		System.out.println("TEST_END: canGetLogDataSource");
+//	}
+//	
+//	@Test
+//	public void canCreateTableOnBasicDataSourceConnection(){
+//		System.out.println("TEST: canCreateTableOnBasicDataSourceConnection");
+//		assertNotNull("Builder was null", builder);
+//		String dbName = "A Special Database"; // key for looking up the datasourceobject.
+//		try {
+//			builder.create(dbName, "jdbc:h2:mem:fusion3", "sa", "sa", "org.h2.Driver");
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//			fail("An exception occurred creating the datasource.");
+//		}
+//		
+//		assertNotNull("Datasource registry null", registry);
+//		
+//		DataSourcePooledJdbc obj = (DataSourcePooledJdbc) registry.get(dbName);
+//		assertNotNull("DataSourcePooledJdbc was null after registry.get", obj);
+//		
+//		assertEquals(DataSourceType.DS_TYPE_JDBC, obj.getDataSourceType());
+//		assertTrue(obj instanceof DataSourcePooledJdbc);
+//		DataSourcePooledJdbc ds = (DataSourcePooledJdbc)obj;
+//		try (Connection conn = ds.getConnection()) {
+//			try (Statement stmt = conn.createStatement()){
+//				stmt.execute(
+//					"CREATE TABLE actual_wind_total "
+//						+ "(TimeStamp datetime NOT NULL, Wind decimal(28,10) DEFAULT NULL, PRIMARY KEY (TimeStamp));");
+//				stmt.execute("INSERT INTO actual_wind_total VALUES('2009-01-20 05:05:05', 20203.4232);");
+//
+//			}
+//		} catch (SQLException e1) {
+//			e1.printStackTrace();
+//			fail();
+//		}
+//		System.out.println("TEST_END: canCreateTableOnBasicDataSourceConnection");
+//	}
+//	
+//	@Test
+//	public void canCreateTableOnConnection(){
+//		System.out.println("TEST: canCreateTableOnConnection");
+//		DataSourceObject obj = registry.get("pnnl.goss.core.server.runner.datasource.H2TestDataSource");
+//		assertNotNull(obj);
+//		assertEquals(DataSourceType.DS_TYPE_JDBC, obj.getDataSourceType());
+//		assertTrue(obj instanceof DataSourcePooledJdbc);
+//		DataSourcePooledJdbc ds = (DataSourcePooledJdbc)obj;
+//		try (Connection conn = ds.getConnection()) {
+//			try (Statement stmt = conn.createStatement()){
+//				stmt.execute(
+//					"CREATE TABLE actual_wind_total "
+//						+ "(TimeStamp datetime NOT NULL, Wind decimal(28,10) DEFAULT NULL, PRIMARY KEY (TimeStamp));");
+//				stmt.execute("INSERT INTO actual_wind_total VALUES('2009-01-20 05:05:05', 20203.4232);");
+//
+//			}
+//		} catch (SQLException e1) {
+//			e1.printStackTrace();
+//			fail();
+//		}
+//		System.out.println("TEST_END: canCreateTableOnConnection");
+//	}
+//	
+//	
+//	@After
+//	public void after(){
+//		cleanUp(this);
+//	}
+//}
