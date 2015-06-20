0dd2eed78f7fe718bf6480c4623a649062052a9c
Craig Allwardt <craig.allwardt@pnnl.gov>
2015-06-19 21:26:16 -0700

    Added server command to allow looking for the number of connected clients.

--- pnnl.goss.core/src/pnnl/goss/core/server/impl/Commands.java
+++ pnnl.goss.core/src/pnnl/goss/core/server/impl/Commands.java
@@ -1,10 +1,14 @@
 package pnnl.goss.core.server.impl;
 
+import java.util.Map.Entry;
+
 import org.apache.felix.dm.annotation.api.Component;
 import org.apache.felix.dm.annotation.api.Property;
 import org.apache.felix.dm.annotation.api.ServiceDependency;
 import org.apache.felix.service.command.CommandProcessor;
 
+import pnnl.goss.core.Client.PROTOCOL;
+import pnnl.goss.core.ClientFactory;
 import pnnl.goss.core.security.AuthorizationHandler;
 import pnnl.goss.core.server.DataSourceRegistry;
 import pnnl.goss.core.server.RequestHandler;
@@ -15,7 +19,8 @@ import pnnl.goss.core.server.RequestUploadHandler;
 
 @Component(properties = {
 		@Property(name=CommandProcessor.COMMAND_SCOPE, value="gs"),
-		@Property(name=CommandProcessor.COMMAND_FUNCTION, value={"listHandlers", "listDataSources", "help"})}, //, "echo", "getEchoHandler"})},
+		@Property(name=CommandProcessor.COMMAND_FUNCTION, value={"listHandlers", 
+				"listDataSources", "showClientConnections", "help"})},
 		provides=Object.class
 )
 public class Commands {
@@ -24,6 +29,8 @@ public class Commands {
 	private volatile RequestHandlerRegistry registry;
 	@ServiceDependency
 	private volatile DataSourceRegistry dsRegistry;
+	@ServiceDependency
+	private volatile ClientFactory clientFactory;
 	
 	public void help(){
 		StringBuilder sb = new StringBuilder();
@@ -31,6 +38,14 @@ public class Commands {
 		sb.append("  listDataSources - Lists the known datasources that have been registered with the server\n");
 		sb.append("  listHandlers - Lists the known request handlers that have been registered with the server.\n");
 		System.out.println(sb.toString());
+	}
+	
+	public void showClientConnections(){
+		
+		for(Entry<String, PROTOCOL> c: clientFactory.list().entrySet()){
+			System.out.println("Client id: " + c.getKey() + 
+					" protocol " + c.getValue().toString());			
+		}
 	}
 	
 	public void listDataSources(){
