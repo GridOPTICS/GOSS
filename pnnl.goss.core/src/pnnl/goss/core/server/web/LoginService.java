5e9063e160e45dffdd68f968f8cb90f0e19a574b
Craig Allwardt <craig.allwardt@pnnl.gov>
2015-06-26 22:05:21 -0700

    Added the ability to retrieve the servlet context from the rest request.

--- pnnl.goss.core/src/pnnl/goss/core/server/web/LoginService.java
+++ pnnl.goss.core/src/pnnl/goss/core/server/web/LoginService.java
@@ -1,9 +1,11 @@
 package pnnl.goss.core.server.web;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 
 import org.apache.shiro.authc.AuthenticationException;
@@ -24,11 +26,12 @@ public class LoginService {
 	@POST
 	@Consumes({MediaType.APPLICATION_JSON, MediaType.TEXT_XML})
 	@Produces(MediaType.APPLICATION_JSON)
-	public String authenticate(UsernamePasswordToken params){
+	public String authenticate(@Context HttpServletRequest request, UsernamePasswordToken params){
 		String sessionToken = null;
 		try{
+			@SuppressWarnings("unused")
 			AuthenticationInfo info = securityManager.authenticate(params);
-			sessionToken = tokenMap.registerIdentifier("ipaddress", params.getUsername());
+			sessionToken = tokenMap.registerIdentifier(request.getRemoteAddr(), params.getUsername());
 			
 		} catch(AuthenticationException e){
 			return "{\"error\": \"Invalid Login\"}";
