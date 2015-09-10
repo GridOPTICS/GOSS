0f0bf17863a104cfe6dbd993e8dd01515526c3c8
Craig Allwardt <craig.allwardt@pnnl.gov>
2015-09-10 15:01:06 -0700

    Activator registers the login and xdomain filters.

--- pnnl.goss.core/src/pnnl/goss/core/server/web/Activator.java
+++ pnnl.goss.core/src/pnnl/goss/core/server/web/Activator.java
@@ -1,5 +1,9 @@
 package pnnl.goss.core.server.web;
 
+import java.util.Hashtable;
+
+import javax.servlet.Filter;
+
 import org.apache.felix.dm.DependencyActivatorBase;
 import org.apache.felix.dm.DependencyManager;
 import org.apache.felix.http.api.ExtHttpService;
@@ -14,8 +18,29 @@ public class Activator extends DependencyActivatorBase {
 	private static String WEB_CONFIG_PID = "pnnl.goss.core.server.web";
 	
 	@Override
+	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public void init(BundleContext context, DependencyManager manager)
 			throws Exception {
+		
+		
+		Hashtable xDomainProps = new Hashtable();
+		xDomainProps.put("pattern", ".*");
+		xDomainProps.put("service.ranking", 10);
+		
+		// Try and keep httpcontext of gosscontext across the board.
+		Hashtable loggedInFilterProps = new Hashtable();
+		loggedInFilterProps.put("pattern", "/.*");
+		loggedInFilterProps.put("contextId", "GossContext");
+		
+		manager.add(createComponent()
+				.setInterface(Filter.class.getName(), xDomainProps)
+				.setImplementation(XDomainFilter.class));
+		
+		manager.add(createComponent()
+				.setInterface(Filter.class.getName(),loggedInFilterProps)
+				.setImplementation(LoggedInFilter.class)
+					.add(createServiceDependency()
+						.setService(TokenIdentifierMap.class)));
 		
 		manager.add(createComponent()
 				.setInterface(Object.class.getName(), null)
@@ -23,18 +48,6 @@ public class Activator extends DependencyActivatorBase {
 				//.setCallbacks("added", "removed", null, null)
 				.add(createServiceDependency()
 						.setService(SecurityManager.class))
-				.add(createServiceDependency()
-						.setService(TokenIdentifierMap.class)));
-		
-		manager.add(createComponent()
-				.setImplementation(XDomainFilter.class)
-				.add(createServiceDependency()
-						.setService(ExtHttpService.class)));
-		
-		manager.add(createComponent()
-				.setImplementation(LoggedInFilter.class)
-				.add(createServiceDependency()
-						.setService(ExtHttpService.class))
 				.add(createServiceDependency()
 						.setService(TokenIdentifierMap.class)));
 		
@@ -47,7 +60,7 @@ public class Activator extends DependencyActivatorBase {
 	@Override
 	public void destroy(BundleContext context, DependencyManager manager)
 			throws Exception {
-		// nop
+		// noop
 	}
 }
 
