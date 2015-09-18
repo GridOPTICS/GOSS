0fdafaa0dac80dba6c8bbbf2d7b3caf217b7b052
Craig <craig.allwardt@pnnl.gov>
2015-09-18 15:57:05 -0700

    Added the ability to register datasources directly through configuration property files.

---
+++ pnnl.goss.core/src/pnnl/goss/core/server/impl/PooledSqlServiceFactory.java
@@ -0,0 +1,125 @@
+package pnnl.goss.core.server.impl;
+
+import java.util.Dictionary;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.apache.felix.dm.DependencyManager;
+import org.apache.felix.dm.annotation.api.Component;
+import org.apache.felix.dm.annotation.api.Inject;
+import org.apache.felix.dm.annotation.api.Property;
+import org.apache.felix.dm.annotation.api.ServiceDependency;
+import org.osgi.framework.Constants;
+import org.osgi.service.cm.ConfigurationException;
+import org.osgi.service.cm.ManagedServiceFactory;
+
+import pnnl.goss.core.server.DataSourceBuilder;
+import pnnl.goss.core.server.DataSourceObject;
+import pnnl.goss.core.server.DataSourcePooledJdbc;
+import pnnl.goss.core.server.DataSourceRegistry;
+import pnnl.goss.core.server.TokenIdentifierMap;
+
+@Component(
+	properties=@Property(
+			name=Constants.SERVICE_PID,
+			value="pnnl.goss.sql.datasource")
+)
+public class PooledSqlServiceFactory implements ManagedServiceFactory{
+
+	@Inject
+	private volatile DependencyManager dm;
+
+//	@ServiceDependency
+//	private volatile DataSourceRegistry dsRegistry;
+
+	// Map of service pid to the actual component.  Note we use long form
+	// of component because it is different than the annotation component
+	// used on the top of the class.
+	private final Map<String, org.apache.felix.dm.Component> components = new ConcurrentHashMap<>();
+
+	//private final Map<String, String> pidToDsName = new ConcurrentHashMap<>();
+
+	@Override
+	public String getName() {
+		return "Pooled Sql Service Factory";
+	}
+
+	private boolean isRequiredKey(String k){
+		switch (k){
+		case DataSourceBuilder.DATASOURCE_USER:
+		case DataSourceBuilder.DATASOURCE_PASSWORD:
+		case DataSourceBuilder.DATASOURCE_URL:
+		case "name":
+			return true;
+
+		default:
+			return false;
+		}
+	}
+
+	@Override
+	public void updated(String pid, Dictionary<String, ?> properties) throws ConfigurationException {
+		// Required values are
+		String[] reqKeys = {"name",
+				DataSourceBuilder.DATASOURCE_USER,
+				DataSourceBuilder.DATASOURCE_PASSWORD,
+				DataSourceBuilder.DATASOURCE_URL};
+		String errorString = "";
+		Map<String, String> props = new HashMap<>();
+		Map<String, String> otherProps = new HashMap<>();
+
+		Enumeration<String> keys = properties.keys();
+
+		while(keys.hasMoreElements()){
+			String key= keys.nextElement();
+
+			String value = (String)properties.get(key);
+
+			if (isRequiredKey(key)){
+				if (value == null || value.isEmpty()){
+					throw new ConfigurationException(key, "Must be specified!");
+				}
+				props.put(key, value);
+			}
+			else{
+				if (value != null && value.isEmpty()){
+					otherProps.put(key, value);
+				}
+			}
+		}
+
+		String datasourceDriver = "com.mysql.jdbc.Driver";
+		if (otherProps.containsKey(DataSourceBuilder.DATASOURCE_DRIVER)){
+			datasourceDriver = otherProps.get(DataSourceBuilder.DATASOURCE_DRIVER);
+			otherProps.remove(DataSourceBuilder.DATASOURCE_DRIVER);
+		}
+
+		PooledSqlServiceImpl service = new PooledSqlServiceImpl(
+				props.get("name"),
+				props.get(DataSourceBuilder.DATASOURCE_URL),
+				props.get(DataSourceBuilder.DATASOURCE_USER),
+				props.get(DataSourceBuilder.DATASOURCE_PASSWORD),
+				datasourceDriver, otherProps);
+
+		org.apache.felix.dm.Component c = dm.createComponent()
+				.setInterface(DataSourceObject.class.getName(), null).setImplementation(service);
+
+		components.put(pid, c);
+		dm.add(c);
+
+		//pidToDsName.put(pid, props.get("name"));
+		dsRegistry.add(props.get("name"), service);
+
+	}
+
+	@Override
+	public void deleted(String pid) {
+		//dsRegistry.remove(pidToDsName.get(pid));
+		//pidToDsName.remove(pid);
+		dm.remove(components.remove(pid));
+	}
+
+
+}
