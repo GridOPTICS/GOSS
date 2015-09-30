0a96d443cb3a24a46282486f3473017d65587dae
Craig Allwardt <craig.allwardt@pnnl.gov>
2015-09-30 14:49:31 -0700

    Updated LoginTestService to be what should pass as the standard web service paradime.

--- pnnl.goss.core/src/pnnl/goss/core/server/web/LoginTestService.java
+++ pnnl.goss.core/src/pnnl/goss/core/server/web/LoginTestService.java
@@ -7,16 +7,40 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
-@Path("/loginTest")
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
+@Path("/api")
 public class LoginTestService {
+	
+	@Context 
+	private HttpServletRequest request;
 		
 	@POST
+	@Path("/echo")
 	@Consumes({MediaType.APPLICATION_JSON, MediaType.TEXT_XML})
 	@Produces(MediaType.APPLICATION_JSON)
-	public String authenticate(@Context HttpServletRequest request){
+	public Response runTest(String body){
 		
-		return "{\"status\": \"Success\"}";
+		Gson gson = new Gson();
+		JsonObject bodyObj = null;
+		JsonObject obj = new JsonObject();
+		
+		try{
+			bodyObj = gson.fromJson(body, JsonObject.class);
+			obj.add("data", bodyObj);
+		}
+		catch(Exception ex){
+			obj.addProperty("data", "Non JSON :"+body);
+		}
+		
+		obj.addProperty("Status", "Success");
+		
+		
+		return Response.status(Status.OK).entity(obj.toString()).build();
 	}
 	
 
